3248c3deb3a1071870cd36e2c3de9d11c052556d
Nicolas Vo <nicvogue@gmail.com>
2022-08-08 19:31:10 -0700

    Create sound data js export
    Import into pad component to start building audio

--- components/fcc/drummachine/Pad.js
+++ components/fcc/drummachine/Pad.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
+import SoundList from './soundList';
 
 import styles from '../../../styles/fcc/DrumMachine.module.css';
 
-const Pad = ({ char, index, bank }) => {
-
-
-	const srcString = `/assets/fcc/DrumMachine/${ bank === 2 ? "synth": bank === 1 ? "mafia" : "flume"}/`
+const Pad = ({ char, padIndex, bank }) => {
+	const fileString = `${SoundList[bank].sounds[padIndex]}`;
+	const dirString = `${ bank === 2 ? "synth": bank === 1 ? "mafia" : "flume"}`;
+	const srcString = `/assets/fcc/DrumMachine/${dirString}/${fileString}.mp3`;
 
 	return (
 		<div id={char} className={styles.pad}>
 			<p>{char}</p>
+			<p>{fileString}</p>
 		</div>
 	);
 };
---
+++ components/fcc/drummachine/soundList.js
@@ -0,0 +1,47 @@
+const SoundList =
+	[
+		{
+			"name": "FlumeSounds",
+			"sounds": [
+				"Granular Twinkle",
+				"Mario Coin Block",
+				"Zelda Boom Bap",
+				"Jewel Flip",
+				"Weekend Morning",
+				"Zelda Forest",
+				"Daze 22.00, v.2.0",
+				"Basic Beatch",
+				"ChonkStomp"
+			]
+		},
+		{
+			"name": "Hip Hop @186 BPM",
+			"sounds": [
+				"Kick 01",
+				"Snare 01",
+				"Kick 02",
+				"Snare 02",
+				"Closed Hat",
+				"Bassline",
+				"Sub 01",
+				"Sub 02",
+				"Meme"
+			]
+		},
+		{
+			"name": "Synths @100 BPM",
+			"sounds": [
+				"Kick",
+				"Snare",
+				"Ride",
+				"Hat",
+				"Vinyl Sound",
+				"Chords",
+				"Bassline",
+				"Arpeg",
+				"Bass Hit"
+			]
+		}
+	]
+
+export default SoundList;
