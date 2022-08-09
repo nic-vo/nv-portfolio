60620b6d6648c03ad5496e4f5838481a3936e4a3
Nicolas Vo <nicvogue@gmail.com>
2022-08-09 14:49:22 -0700

    Add significant function to DrumMachine
    Keypress activation of sound works even on repeated presses
    Now holds sound/bank names for display
    Passes master volume float as prop to all pads

--- pages/fcc/drum-machine.js
+++ pages/fcc/drum-machine.js
@@ -7,28 +7,54 @@ import styles from '../../styles/fcc/DrumMachine.module.css';
 
 const chars = ["q", "w", "e", "a", "s", "d", "z", "x", "c"]
 
-const Drumpad = () => {
+const DrumMachine = () => {
 	/*
 			Keeps track of sound last played, active sound bank, master volume,
 	*/
-	const [activeSound, setActiveSound] = useState("");
-	const [activeBank, setActiveBank] = useState(0);
+	const [displaySound, setDisplaySound] = useState("");
+	const [activeKey, setActiveKey] = useState("test")
+	const [bankIndex, setBankIndex] = useState(0);
 	const [mVolume, setMVolume] = useState(1);
 
-	const setActiveSoundHandler = (sound) => {
-		setActiveSound(sound);
+	// bank name / dir based on bankIndex
+	const bank = `${bankIndex === 2 ? "Synths @100 BPM" : bankIndex === 1 ? "Hip Hop @186 BPM" : "FlumeSounds"}`
+
+	const setDisplaySoundHandler = (sound, playing = true) => {
+		// Updates displaySound based on last activated sound
+		// Clears upon the last sound ending
+		if (sound === displaySound) {
+			if (playing === false) { setDisplaySound(""); }
+		} else if (playing === true) {
+			setDisplaySound(sound);
+		}
 	};
+
+	const mVolumeonInputHandler = (e) => {
+		setMVolume(e.target.value);
+	};
+
+	const keyPressHandler = (e) => {
+		setActiveKey(e.key);
+		// Set timeout that instantly resets the activate prop to the active key
+		const pulser = setTimeout(() => {
+			setActiveKey("");
+		});
+	}
 
 	return (
 		<>
 			<h1>Drum Machine</h1>
-			<div className={styles.padGrid} tabIndex={0}>
+			<input type="range" min="0" max="1" step="0.05" value={mVolume} onInput={mVolumeonInputHandler} />
+			<p>Active sound: {displaySound}</p>
+			<div className={styles.grid} tabIndex="0" onKeyPress={keyPressHandler}>
 				{chars.map((char, index) => {
 					return <Pad
 						char={char}
 						padIndex={index}
-						bank={activeBank}
-						setActiveSound={setActiveSoundHandler}
+						bank={bank}
+						mVolume={mVolume}
+						activate={activeKey === char ? true : false}
+						setDisplaySound={setDisplaySoundHandler}
 						key={`pad-${index}`}
 					/>
 				})}
@@ -37,4 +63,4 @@ const Drumpad = () => {
 	);
 };
 
-export default Drumpad;
+export default DrumMachine;
