fe85293bcd1aa39ae81fe9547fd7666067b70785
Nicolas Vo <nicvogue@gmail.com>
2022-08-09 15:05:08 -0700

    Remove unnecessary depth from fiie list

--- components/fcc/drummachine/soundList.js
+++ components/fcc/drummachine/soundList.js
@@ -1,47 +1,40 @@
 const SoundList =
-	[
-		{
-			"name": "FlumeSounds",
-			"sounds": [
-				"Granular Twinkle",
-				"Mario Coin Block",
-				"Zelda Boom Bap",
-				"Jewel Flip",
-				"Weekend Morning",
-				"Zelda Forest",
-				"Daze 22.00, v.2.0",
-				"Basic Beatch",
-				"ChonkStomp"
-			]
-		},
-		{
-			"name": "Hip Hop @186 BPM",
-			"sounds": [
-				"Kick 01",
-				"Snare 01",
-				"Kick 02",
-				"Snare 02",
-				"Closed Hat",
-				"Bassline",
-				"Sub 01",
-				"Sub 02",
-				"Meme"
-			]
-		},
-		{
-			"name": "Synths @100 BPM",
-			"sounds": [
-				"Kick",
-				"Snare",
-				"Ride",
-				"Hat",
-				"Vinyl Sound",
-				"Chords",
-				"Bassline",
-				"Arpeg",
-				"Bass Hit"
-			]
-		}
+
+{
+	"FlumeSounds": [
+		"Granular Twinkle",
+		"Mario Coin Block",
+		"Zelda Boom Bap",
+		"Jewel Flip",
+		"Weekend Morning",
+		"Zelda Forest",
+		"Daze 22.00 v. 2.0",
+		"Basic Beatch",
+		"ChonkStomp"
+	],
+	"Hip Hop @186 BPM": [
+		"Kick 01",
+		"Snare 01",
+		"Kick 02",
+		"Snare 02",
+		"Closed Hat",
+		"Bassline",
+		"Sub 01",
+		"Sub 02",
+		"Meme"
+	],
+	"Synths @100 BPM": [
+		"Kick",
+		"Snare",
+		"Ride",
+		"Hat",
+		"Vinyl Sound",
+		"Chords",
+		"Bassline",
+		"Arpeg",
+		"Bass Hit"
 	]
+}
+
 
 export default SoundList;
