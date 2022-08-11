1f42464b79d2f42f114b0c50af165600c17f2481
Nicolas Vo <nicvogue@gmail.com>
2022-08-10 17:41:32 -0700

    Add master inputs to control panel
    Use the handlers passed from parent to control global volume, sound bank, global mute, and pulse stopAll

--- components/fcc/drummachine/ControlPanel.js
+++ components/fcc/drummachine/ControlPanel.js
@@ -5,8 +5,17 @@ import styles from '../../../styles/fcc/DrumMachine/ControlPanel.module.css';
 const ControlPanel = ({ displaySound, mVolume, bankIndex, bank, muteAll, mVolumeHandler, bankIndexHandler, stopAllHandler, muteAllHandler }) => {
 	return (
 		<div>
-			<label for="mVolume">Master Volume<input id="mVolume" type="range" min="0" max="1" step="0.05" value={mVolume} onInput={mVolumeHandler} /> <p>{mVolume}</p></label>
+			<label htmlFor="mVolume">Master Volume
+				<input id="mVolume" type="range" min="0" max="1" step="0.05" value={mVolume} onInput={mVolumeHandler} />
+				<p>{mVolume}</p>
+			</label>
+			<label htmlFor="bankIndex">Sound Bank
+				<input id="bankIndex" type="range" min="0" max="2" step="1" value={bankIndex} onInput={bankIndexHandler} />
+				<p>{bank}</p>
+			</label>
 			<p>Active sound: {displaySound}</p>
+			<button onClick={stopAllHandler}>STOP ALL</button>
+			<button onClick={muteAllHandler}>MUTE ALL</button>
 		</div>
 	);
 };
