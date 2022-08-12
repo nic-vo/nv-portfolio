32faeffae224316ce6f066b44eb1170dcabb7a2e
Nicolas Vo <nicvogue@gmail.com>
2022-08-11 23:41:19 -0700

    Remove unnecessary bank index prop from control panel
    Was basically a duplicate prop for bank name
    Replace by using the existing index to determine bank name

--- components/fcc/drummachine/ControlPanel.js
+++ components/fcc/drummachine/ControlPanel.js
@@ -2,16 +2,18 @@ import React from 'react'
 
 import styles from '../../../styles/fcc/DrumMachine/ControlPanel.module.css';
 
-const ControlPanel = ({ displaySound, mVolume, bankIndex, bank, muteAll, mVolumeHandler, bankIndexHandler, stopAllHandler, muteAllHandler }) => {
+const bankNames = ["FlumeSounds", "Hip Hop @186 BPM", "Synths @100 BPM"];
+
+const ControlPanel = ({ displaySound, mVolume, bank, muteAll, mVolumeHandler, setBankHandler, stopAllHandler, muteAllHandler }) => {
 	return (
 		<div>
 			<label htmlFor="mVolume">Master Volume
 				<input id="mVolume" type="range" min="0" max="1" step="0.05" value={mVolume} onInput={mVolumeHandler} />
 				<p>{mVolume}</p>
 			</label>
-			<label htmlFor="bankIndex">Sound Bank
-				<input id="bankIndex" type="range" min="0" max="2" step="1" value={bankIndex} onInput={bankIndexHandler} />
-				<p>{bank}</p>
+			<label htmlFor="bank">Sound Bank
+				<input id="bank" type="range" min="0" max="2" step="1" value={bank} onInput={setBankHandler} />
+				<p>{bankNames[bank]}</p>
 			</label>
 			<p>Active sound: {displaySound}</p>
 			<button onClick={stopAllHandler}>STOP ALL</button>
