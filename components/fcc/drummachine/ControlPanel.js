cb60e913decbdb7a7c957e9d8bbb75306754cf66
Nicolas Vo <nicvogue@gmail.com>
2022-08-16 09:23:09 -0700

    Add react-icons to control panel
    Adjust DOM nesting

--- components/fcc/drummachine/ControlPanel.js
+++ components/fcc/drummachine/ControlPanel.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FaVolumeMute, FaStop } from 'react-icons/fa';
 
 import machineStyles from '../../../styles/fcc/DrumMachine/DrumMachine.module.css';
 import controlStyles from '../../../styles/fcc/DrumMachine/ControlPanel.module.css';
@@ -7,18 +8,21 @@ const bankNames = ["FlumeSounds", "Hip Hop @186 BPM", "Synths @100 BPM"];
 
 const ControlPanel = ({ displaySound, mVolume, bank, muteAll, mVolumeHandler, setBankHandler, stopAllHandler, muteAllHandler }) => {
 	return (
-		<div>
-			<label htmlFor="mVolume">Master Volume
-				<input id="mVolume" type="range" min="0" max="1" step="0.05" value={mVolume} onInput={mVolumeHandler} />
-				<p>{mVolume}</p>
-			</label>
-			<label htmlFor="bank">Sound Bank
-				<input id="bank" type="range" min="0" max="2" step="1" value={bank} onInput={setBankHandler} />
-				<p>{bankNames[bank]}</p>
-			</label>
-			<p>Active: {displaySound}</p>
-			<button className={`${machineStyles.button} ${machineStyles.stop}`} onClick={stopAllHandler}>STOP ALL</button>
-			<button className={` ${machineStyles.button} ${muteAll ? machineStyles.muteon : machineStyles.mute}`} onClick={muteAllHandler}>MUTE ALL</button>
+		<div className={controlStyles.panel}>
+			<div className={controlStyles.sounds}>
+				<div className={controlStyles.soundDisplay}>
+					<p>{bankNames[bank]}: {displaySound}</p>
+				</div>
+				<label htmlFor="bank">Sound Bank<input id="bank" type="range" min="0" max="2" step="1" value={bank} onInput={setBankHandler} /></label>
+			</div>
+			<div className={controlStyles.playback}>
+				<label htmlFor="mVolume">Master Volume
+					<input id="mVolume" type="range" min="0" max="1" step="0.05" value={mVolume} onInput={mVolumeHandler} />
+				</label>
+				<button className={`${machineStyles.button} ${machineStyles.stop}`} onClick={stopAllHandler}><FaStop /></button>
+				<button className={`${machineStyles.button} ${muteAll ? machineStyles.muteon : machineStyles.mute}`} onClick={muteAllHandler}><FaVolumeMute /></button>
+			</div>
+
 		</div>
 	);
 };
