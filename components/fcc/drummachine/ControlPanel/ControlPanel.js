8b120ab9822bd352db56379147806ae4e3429b40
Nicolas Vo <nicvogue@gmail.com>
2022-08-18 16:45:45 -0700

    Rearrange control panel so display and controls are separate
    Add new svg for global mute based on state

--- components/fcc/drummachine/ControlPanel/ControlPanel.js
+++ components/fcc/drummachine/ControlPanel/ControlPanel.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaVolumeMute, FaStop } from 'react-icons/fa';
+import { FaVolumeMute, FaStop, FaMusic, FaVolumeUp, FaVolumeOff } from 'react-icons/fa';
 
 import machineStyles from '../../../../styles/fcc/DrumMachine/DrumMachine.module.css';
 import controlStyles from './ControlPanel.module.css';
@@ -9,21 +9,21 @@ const bankNames = ["FlumeSounds", "Hip Hop @186 BPM", "Synths @100 BPM"];
 const ControlPanel = ({ displaySound, mVolume, bank, muteAll, mVolumeHandler, setBankHandler, stopAllHandler, muteAllHandler }) => {
 	return (
 		<div className={controlStyles.panel}>
-			<div className={controlStyles.sounds}>
-				<div className={controlStyles.soundDisplay}>
-					<p>{bankNames[bank]}:</p>
-					<p>{displaySound}</p>
+			<div className={controlStyles.display}>
+				<p>{bankNames[bank]}:</p>
+				<p>{displaySound}</p>
+			</div>
+			<div className={controlStyles.controls}>
+				<div className={controlStyles.sliders}>
+					<label htmlFor="bank"><FaMusic /><input id="bank" type="range" min="0" max="2" step="1" value={bank} onInput={setBankHandler} /></label>
+					<label htmlFor="mVolume"><FaVolumeUp /><input id="mVolume" type="range" min="0" max="1" step="0.05" value={mVolume} onInput={mVolumeHandler} />
+					</label>
 				</div>
-				<label htmlFor="bank">Sound Bank<input id="bank" type="range" min="0" max="2" step="1" value={bank} onInput={setBankHandler} /></label>
+				<div className={controlStyles.buttons}>
+					<button className={`${machineStyles.button} ${machineStyles.stop}`} onClick={stopAllHandler}><FaStop /></button>
+					<button className={`${machineStyles.button} ${muteAll ? machineStyles.muteon : machineStyles.mute}`} onClick={muteAllHandler}>{muteAll === true ? <FaVolumeMute /> : <FaVolumeOff />}</button>
+				</div>
 			</div>
-			<div className={controlStyles.playback}>
-				<label htmlFor="mVolume">Master Volume
-					<input id="mVolume" type="range" min="0" max="1" step="0.05" value={mVolume} onInput={mVolumeHandler} />
-				</label>
-				<button className={`${machineStyles.button} ${machineStyles.stop}`} onClick={stopAllHandler}><FaStop /></button>
-				<button className={`${machineStyles.button} ${muteAll ? machineStyles.muteon : machineStyles.mute}`} onClick={muteAllHandler}><FaVolumeMute /></button>
-			</div>
-
 		</div>
 	);
 };
