5d6b0699097751c230ef0ac77de18be470237917
Nicolas Vo <nicvogue@gmail.com>
2022-08-26 12:08:04 -0700

    Disable history item button when a history chunk has been input

--- components/fcc/calculator/History/History.js
+++ components/fcc/calculator/History/History.js
@@ -4,11 +4,12 @@ import { FaArrowRight } from 'react-icons/fa';
 const History = ({
 	history,
 	pickHistory,
-	clearHistory
+	clearHistory,
+	oldAllowed
 }) => {
 	return (
 		<div>
-			{history.map((item, index) => { return <div key={`his-${index}`}><p>{item[0]} = {item[1]}</p><button value={item[1]} onClick={pickHistory}>Enter</button></div> })}
+			{history.map((item, index) => { return <div key={`his-${index}`}><p>{item[0]} = {item[1]}</p><button value={item[1]} onClick={pickHistory} disabled={!oldAllowed}>Enter</button></div> })}
 			<button onClick={clearHistory}>Clear History</button>
 		</div >
 	);
