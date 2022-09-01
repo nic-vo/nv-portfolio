e5b4600979ce43b080b94a2e435f44d2bd1e2d81
Nicolas Vo <nicvogue@gmail.com>
2022-09-01 13:19:05 -0700

    Add sploding useEffect to Keypad
    Maps an array to a string in the style attribute object on the button
    Controls rotate3d and translate3d
    Did my own vec normalization just cuz

--- components/fcc/calculator/Keypad/Keypad.js
+++ components/fcc/calculator/Keypad/Keypad.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 
 import keyStyles from './Keypad.module.css';
 
@@ -6,11 +6,57 @@ const Keypad = ({
 	keyId,
 	keyVal,
 	content,
-	handler
+	handler,
+	splode
 }) => {
 
+	const [sploded, setSploded] = useState([[0, 0, 0, 0], [0, 0, 0]]);
+
+	const resetSploded = () => {
+		setSploded([[0, 0, 0, 0], [0, 0, 0]]);
+	};
+
+	const newRan = () => {
+		const negFifty = () => {
+			if (Math.random() < 0.5) { return "-" };
+			return "";
+		};
+		return `${negFifty()}${Math.random().toFixed(4)}`;
+	};
+
+	const normalizedVecs = () => {
+		const newArr = [parseFloat(newRan()), parseFloat(newRan()), parseFloat(newRan())]
+		const total = newArr.reduce((newTotal, current) => {
+			return Math.abs(current) + Math.abs(newTotal);
+		});
+		return newArr.map((vec) => {
+			return vec/total;
+		});
+	};
+
+	const newTran = () => {
+		return `${newRan() * 150}px`;
+	};
+
+	const newSploded = () => {
+		setSploded([[...normalizedVecs(), parseFloat(newRan()).toFixed(5)], [newTran(), newTran(), newTran()]]);
+	}
+
+	useEffect(() => {
+		if (splode === false) { return resetSploded(); }
+		newSploded();
+	}, [splode]);
+
 	return (
-		<button className={`${keyStyles.keypad}${keyId === "clear" ? ` ${keyStyles.clear}` : ""}`} id={keyId} value={keyVal} onClick={handler} style={{ "gridArea": keyId }}>
+		<button
+			id={keyId}
+			value={keyVal}
+			onClick={handler}
+			className={`${keyStyles.keypad}${keyId === "clear" ? ` ${keyStyles.clear}` : ""}`}
+			style={{
+				"gridArea": keyId,
+				"transform": `rotate3d(${sploded[0][0]}, ${sploded[0][1]}, ${sploded[0][2]}, ${sploded[0][3]*360}deg) translate3d(${sploded[1][0]},${sploded[1][1]},${sploded[1][2]})`
+			}}>
 			{content}
 		</button>
 	);
