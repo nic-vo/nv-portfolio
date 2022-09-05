21109228cb34ffa569f8a65be159dbe942cde394
Nicolas Vo <nicvogue@gmail.com>
2022-09-04 17:18:28 -0700

    Move history styles over to history css module

--- components/fcc/calculator/History/History.js
+++ components/fcc/calculator/History/History.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FaArrowRight } from 'react-icons/fa';
 
-import calcStyles from '../../../../styles/fcc/Calculator/Calculator.module.css';
+import histStyles from './History.module.css';
 
 const History = ({
 	history,
@@ -10,11 +10,11 @@ const History = ({
 	oldAllowed
 }) => {
 	return (
-		<div className={calcStyles.history}>
+		<div className={histStyles.history}>
 			<button onClick={clearHistory}>Clear History</button>
 			{history.map((item, index) => {
 				return (
-					<div className={calcStyles.historyitem} key={`his-${index}`}>
+					<div className={histStyles.historyitem} key={`his-${index}`}>
 						<p>{item[0]} = {item[1]}</p>
 						<button value={item[1]} onClick={pickHistory} disabled={!oldAllowed}>Enter</button>
 					</div>)
