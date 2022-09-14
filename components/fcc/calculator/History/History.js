3f907791e288b1822fc72818f7b5c668650e8aa7
Nicolas Vo <nicvogue@gmail.com>
2022-09-14 14:36:55 -0700

    Move Calculator comp to comp folder
    Move css module with it
    Fix all imports and adjust export from dir

--- components/fcc/calculator/History/History.js
+++ components/fcc/calculator/History/History.js
@@ -1,4 +1,4 @@
-import React, { memo } from 'react';
+import React from 'react';
 import { FaUndoAlt } from 'react-icons/fa';
 
 import histStyles from './History.module.css';
@@ -12,15 +12,18 @@ const History = ({
 	return (
 		<div className={histStyles.history}>
 			<button className={histStyles.clearhistory} onClick={clearHistory}>Clear History</button>
-			{history.map((item, index) => {
-				return (
-					<div className={histStyles.historyitem} key={`his-${index}`}>
-						<p>{item[0].replaceAll(/([+*/]|(?<!([+*/\-]|^))-)/g, " $1 ")} = {item[1]}</p>
-						<button value={item[1]} onClick={pickHistory} disabled={!oldAllowed}><FaUndoAlt /></button>
-					</div>)
-			})}
+			{
+				history.map((item, index) => {
+					return (
+						<div className={histStyles.historyitem} key={`his-${index}`}>
+							<p>{item[0].replaceAll(/([+*/]|(?<!([+*/\-]|^))-)/g, " $1 ")} = {item[1]}</p>
+							<button value={item[1]} onClick={pickHistory} disabled={!oldAllowed}><FaUndoAlt /></button>
+						</div>
+					)
+				})
+			}
 		</div >
 	);
 };
 
-export default memo(History);
+export default History;
