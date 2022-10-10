22e3691d0f52bc9e080de79f50c8c1455f10663f
Nicolas Vo <nicvogue@gmail.com>
2022-10-10 14:08:09 -0700

    Begin mockup of nav

--- components/global/Layouts/ProjectLayout.js
+++ components/global/Layouts/ProjectLayout.js
@@ -3,7 +3,22 @@ import Head from "next/head";
 const ProjectLayout = ({ children }) => {
 	return (
 		<>
-			<header><h1>Header</h1></header>
+			<header>
+				<h1>Header</h1>
+				<nav>
+					<ul>
+						<li style={{ listStyle: "none" }}>
+							<a href="/" style={{ listStyle: "none" }}>Back to home</a>
+						</li>
+						<li style={{ listStyle: "none" }}>
+							<a href="/fcc/Calculator" style={{ listStyle: "none" }} >Calculator</a>
+						</li>
+						<li style={{ listStyle: "none" }}>
+							<a href="/fcc/DrumMachine" style={{ listStyle: "none" }} >Drum Machine</a>
+						</li>
+					</ul>
+				</nav>
+			</header>
 			<main>
 				{children}
 			</main>
