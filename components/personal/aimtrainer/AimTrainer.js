dceb074f86f300d1da6a377dd5e231aa9c18056e
Nicolas Vo <nicvogue@gmail.com>
2022-11-02 11:59:33 -0700

    Init several personal projects
    Develop the collatz a little bit

---
+++ components/personal/aimtrainer/AimTrainer.js
@@ -0,0 +1,9 @@
+const AimTrainer = () => {
+	return (
+		<section>
+			This is the aim trainer comp
+		</section>
+	);
+};
+
+export default AimTrainer;
---
+++ pages/personal/ATrainer.js
@@ -0,0 +1,25 @@
+import Head from "next/head";
+
+import { ProjectLayout } from "../../components/global";
+import { AimTrainer } from "../../components/personal";
+
+const ATrainer = () => {
+	return (<>
+		<Head>
+			<title>An Aim Trainer</title>
+			<meta name="description" content="An aim trainer that's really only suited for 2d games like osu!" />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+		<ProjectLayout>
+			<h1>Aim Trainer</h1>
+			<AimTrainer />
+			<section>
+				<p>
+					Description blurb
+				</p>
+			</section>
+		</ProjectLayout>
+	</>);
+};
+
+export default ATrainer;
