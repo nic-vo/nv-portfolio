bf4f74979ba2f7b6a5879c7924f3d5c0261454be
Nicolas Vo <nicvogue@gmail.com>
2022-12-12 15:50:13 -0800

    Modify webpack config to allow for svg as components lib

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
 	reactStrictMode: false,
 	swcMinify: true,
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]s$/,
+			use: ['@svgr/webpack']
+		})
+		return config;
+	}
 };
 
 module.exports = nextConfig;
