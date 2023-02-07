c6406b0a1b7b42b89157437ae03fc44b126ccc1b
Nicolas Vo <nicvogue@gmail.com>
2023-02-07 03:32:46 -0800

    Minor changes to duckies, back burner for now

--- components/homepage/index.js
+++ components/homepage/index.js
@@ -1,4 +1,4 @@
-import Duckies from "./duckies/Duckies";
+import Duckies from './duckies/Duckies';
 import Hero from './Hero/Hero';
 import ProjectCard from "./ProjectCard/ProjectCard";
 import ContactForm from "./ContactForm/ContactForm";
