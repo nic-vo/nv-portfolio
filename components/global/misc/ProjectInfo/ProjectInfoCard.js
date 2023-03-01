1d8dbffff6b79ece1c96e2c48bb55f393db4523f
Nicolas Vo <nicvogue@gmail.com>
2023-02-28 22:40:32 -0800

    Add new techbubble comp and add it to project cards

--- components/global/misc/ProjectInfo/ProjectInfoCard.js
+++ components/global/misc/ProjectInfo/ProjectInfoCard.js
@@ -1,20 +1,41 @@
-import { useState } from "react";
+import TechBubble from '../TechBubble/TechBubble';
 
 import pInfoCardLook from './ProjectInfoCard.module.scss';
 
 const ProjectInfoCard = ({ techs, description, original }) => {
 
+	const { type, link } = original;
+
+	const typeDisplay = type[0].toUpperCase() + type.slice(1);
+
+	let linkClasser;
+	switch (type) {
+		case 'github':
+			linkClasser = pInfoCardLook.github;
+			break;
+		case 'codepen':
+			linkClasser = pInfoCardLook.codepen;
+			break;
+		default:
+			linkClasser = pInfoCardLook.liveLink;
+			break;
+	};
+
 	return (
-		<section className={pInfoCardLook.infoContainer}>
-			<p>Original: <a href={original.link} target='_blank'>{original.type === 'github' ? 'Github' : 'Codepen'}</a></p>
-			<ul className={pInfoCardLook.techList}>
-				{techs.map((tech) => {
-					return (
-						<li className={pInfoCardLook.tech} key={`${tech}`}>{tech}</li>
-					)
-				})}
-			</ul>
-			<p>{description}</p>
+		<section className={pInfoCardLook.container}>
+			<div className={pInfoCardLook.info}>
+				<ul className={pInfoCardLook.techList}>
+					{
+						techs.map((tech) => {
+							return (
+								<TechBubble key={`${tech}`} tech={tech} />
+							)
+						})
+					}
+				</ul>
+				<div className={pInfoCardLook.linkIntro}><p>Link to original:</p><a href={original.link} target='_blank' className={pInfoCardLook.link + ' ' + linkClasser}> {typeDisplay}</a></div>
+			</div>
+			<p className={pInfoCardLook.description}>{description}</p>
 		</section>
 	);
 };
