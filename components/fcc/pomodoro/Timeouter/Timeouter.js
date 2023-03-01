a93e6863d22cc0fa3f3a3cff5a587e0a5ed4024c
Nicolas Vo <nicvogue@gmail.com>
2023-02-28 22:38:06 -0800

    Reduce complexity of pomodoro:
    skipping can only occur when inactive (solves many problems related to closures

--- components/fcc/pomodoro/Timeouter/Timeouter.js
+++ components/fcc/pomodoro/Timeouter/Timeouter.js
@@ -31,21 +31,15 @@ const Timeouter = ({ work, rest, activate, activator, workPhase, workToggle }) =
 	};
 
 	const skipper = () => {
+		if (activate === true) { return };
 		let phaseChange = workPhase === true ? work : rest;
-		if (activate === false) {
-			setCurrentTime(phaseChange * MULTIPLIER);
-		} else {
-			clearTimeout(loop);
-			setLoop(null);
-			setExpected(Date.now() + 1000);
-			setCurrentTime(phaseChange * MULTIPLIER);
-		};
+		clearTimeout(loop);
+		setCurrentTime(phaseChange * MULTIPLIER);
 		workToggle(!workPhase);
 	};
 
 	const activateHandler = () => {
 		if (activate === false) {
-			activator();
 			setExpected(Date.now() + 1000);
 			setLoop(() => {
 				return setTimeout(() => {
@@ -53,43 +47,52 @@ const Timeouter = ({ work, rest, activate, activator, workPhase, workToggle }) =
 				}, 1000);
 			});
 		} else {
-			activator();
 			clearTimeout(loop);
 			setLoop(null);
 			setExpected(null);
 		};
+		activator();
 	};
 
 	useEffect(() => {
 		if (activate === false) { return };
 		const now = Date.now();
 		const diff = now - expected;
-		setExpected(now + 1000);
 		if (currentTime <= 1) {
 			let phaseChange = workPhase === true ? work : rest;
 			setLoop(() => {
 				return setTimeout(() => {
-					setCurrentTime(phaseChange * MULTIPLIER);
 					workToggle(!workPhase);
+					setExpected(now + 1000);
+					setCurrentTime(phaseChange * MULTIPLIER);
 				}, 1000 - diff);
 			});
 		} else {
 			setLoop(() => {
 				return setTimeout(() => {
+					setExpected(now + 1000);
 					setCurrentTime((current) => { return current - 1 });
 				}, 1000 - diff);
 			});
 		};
 	}, [currentTime]);
 
+	// useEffect(() => {
+	// 	clearTimeout(loop);
+	// 	setLoop(null);
+	// 	let phaseChange = workPhase === true ? work : rest;
+	// 	setCurrentTime(phaseChange * MULTIPLIER);
+	// 	return () => { clearTimeout(loop) };
+	// }, [workPhase]);
+
 	// The following two hooks are for accepting timer value changes from parent
 	useEffect(() => {
-		if (workPhase === false || activate === false) { return };
+		if (workPhase === false || activate === true) { return };
 		setCurrentTime(work * MULTIPLIER);
 	}, [work]);
 
 	useEffect(() => {
-		if (workPhase === true || activate === false) { return };
+		if (workPhase === true || activate === true) { return };
 		setCurrentTime(rest * MULTIPLIER);
 	}, [rest]);
 
@@ -112,9 +115,9 @@ const Timeouter = ({ work, rest, activate, activator, workPhase, workToggle }) =
 			<h2 className={timeLook.bigLabel}>{workPhase ? "WORKIN'" : "RESTIN'"}</h2>
 			<p className={timeLook.timeOutput}>{`${currentTime >= 600 ? Math.floor(currentTime / 60) : `0${Math.floor(currentTime / 60)}`}`}:{`${currentTime % 60 >= 10 ? currentTime % 60 : `0${currentTime % 60}`}`}</p>
 			<div className={timeLook.controls}>
-				<button onClick={resetHandler} className={pomoLook.menter} disabled={activate}>Reset</button>
+				<button onClick={resetHandler} disabled={activate} className={pomoLook.menter} >Reset</button>
 				<button onClick={activateHandler} className={pomoLook.menter} >{activate === true ? "Stop" : "Start"}</button>
-				<button onClick={skipper} className={pomoLook.menter}>Skip</button>
+				<button onClick={skipper} disabled={activate} className={pomoLook.menter}>Skip</button>
 			</div>
 			<audio autoPlay={false} src={assetPath + "work.mp3"} id='workaudio' ref={workRef} />
 			<audio autoPlay={false} src={assetPath + "rest.mp3"} id='restaudio' ref={restRef} />
