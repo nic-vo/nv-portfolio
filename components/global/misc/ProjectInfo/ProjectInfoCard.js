30fb885998a5e6c94692edda94d78f138817a9ae
Nicolas Vo <nicvogue@gmail.com>
2023-03-01 00:30:12 -0800

    Rearrange project info to be less weird

--- components/global/misc/ProjectInfo/ProjectInfoCard.js
+++ components/global/misc/ProjectInfo/ProjectInfoCard.js
@@ -23,19 +23,17 @@ const ProjectInfoCard = ({ techs, description, original }) => {
 
 	return (
 		<section className={pInfoCardLook.container}>
-			<div className={pInfoCardLook.info}>
-				<ul className={pInfoCardLook.techList}>
-					{
-						techs.map((tech) => {
-							return (
-								<TechBubble key={`${tech}`} tech={tech} />
-							)
-						})
-					}
-				</ul>
-				<div className={pInfoCardLook.linkIntro}><p>Link to original:</p><a href={original.link} target='_blank' className={pInfoCardLook.link + ' ' + linkClasser}> {typeDisplay}</a></div>
-			</div>
+			<ul className={pInfoCardLook.techList}>
+				{
+					techs.map((tech) => {
+						return (
+							<TechBubble key={`${tech}`} tech={tech} />
+						)
+					})
+				}
+			</ul>
 			<p className={pInfoCardLook.description}>{description}</p>
+			<div className={pInfoCardLook.linkIntro}><p>Link to original:</p><a href={original.link} target='_blank' className={pInfoCardLook.link + ' ' + linkClasser}> {typeDisplay}</a></div>
 		</section>
 	);
 };
