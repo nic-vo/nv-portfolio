61a8b38acc335df2e8f199e20a7f4f8bcfea4dd8
Nicolas Vo <nicvogue@gmail.com>
2023-02-28 23:43:01 -0800

    Add various global components to index.js

--- components/global/index.js
+++ components/global/index.js
@@ -1,9 +1,15 @@
 import ProjectLayout from './layouts/ProjectLayout/ProjectLayout';
 import Spinner from './misc/Spinner/Spinner';
 import Footer from './misc/Footer/Footer';
+import Wip from './misc/Wip/Wip';
+import ImageCarousel from './misc/ImageCarousel/ImageCarousel';
+import TechBubble from './misc/TechBubble/TechBubble';
 
 export {
 	ProjectLayout,
 	Spinner,
-	Footer
+	Footer,
+	Wip,
+	ImageCarousel,
+	TechBubble
 }
