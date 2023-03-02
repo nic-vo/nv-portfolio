e10329e75030a510d18e383c9500c674f22dbd55
Nicolas Vo <nicvogue@gmail.com>
2023-03-01 18:07:02 -0800

    AboutMe default export to index

--- components/homepage/index.js
+++ components/homepage/index.js
@@ -2,10 +2,12 @@ import Duckies from './duckies/Duckies';
 import Hero from './Hero/Hero';
 import ContactForm from './ContactForm/ContactForm';
 import Nav from './Nav/Nav';
+import AboutMe from './AboutMe/AboutMe';
 
 export {
 	Duckies,
 	Hero,
 	Nav,
-	ContactForm
+	ContactForm,
+	AboutMe
 };
