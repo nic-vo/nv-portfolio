48b77ba144f8e98e844d216e7728bfd8b2d2d5f7
Nicolas Vo <nicvogue@gmail.com>
2023-03-02 00:32:28 -0800

    Adjust landing subtitle

--- components/homepage/Hero/Hero.js
+++ components/homepage/Hero/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
 		<section className={heroLook.container}>
 			<h1 className={heroLook.hOne}>
 				<p className={heroLook.name}>Nicolas Vo</p>
-				<p className={heroLook.title}>A Frontend Developer</p>
+				<p className={heroLook.title}>Frontend Developer</p>
 			</h1>
 			<Crosses limit={50} />
 		</section>
