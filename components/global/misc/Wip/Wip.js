596c37e09c3e0c7620059da4bc22f7d4d0161e0b
Nicolas Vo <nicvogue@gmail.com>
2023-03-02 00:33:40 -0800

    Make WIP component fit site look better

--- components/global/misc/Wip/Wip.js
+++ components/global/misc/Wip/Wip.js
@@ -3,8 +3,8 @@ import wipLook from './Wip.module.scss';
 const Wip = () => {
 	return (
 		<div className={wipLook.container}>
-			<h2>WIP</h2>
-			<p>(work in progress)</p>
+			<p className={wipLook.big}>WIP</p>
+			<p className={wipLook.small}>(work in progress)</p>
 		</div>
 	);
 };
