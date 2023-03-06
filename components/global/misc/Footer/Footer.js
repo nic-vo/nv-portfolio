2ffc8beb9a0911faff912d407cfcbef8dc96aef3
Nicolas Vo <nicvogue@gmail.com>
2023-03-06 15:23:36 -0800

    Add copyright to footer w/ name

--- components/global/misc/Footer/Footer.js
+++ components/global/misc/Footer/Footer.js
@@ -3,14 +3,20 @@ import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { ContactForm } from '../../../homepage';
 
 const Footer = ({ version }) => {
+
+	const date = new Date();
+
 	return (
 		<footer className={footLook.footer} id='footer'>
 			<ContactForm />
 			<hr className={footLook.divider} />
-			<p>site v. {version} by nicolas vo</p>
 			<div className={footLook.links}>
 				<a href='https://github.com/nic-vo' target='_blank'><FaGithub /></a>
 				<a href='https://linkedin.com/in/nicolas-vo-685086208' target='_blank'><FaLinkedin /></a>
+			</div>
+			<div className={footLook.textbanner}>
+				<p>v. {version}</p>
+				<p>&copy; {date.getFullYear()} Nicolas Vo</p>
 			</div>
 		</footer>
 	);
