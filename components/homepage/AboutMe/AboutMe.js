8ace023aae76c3c36dcf6d8ac080a195ccd127ea
Nicolas Vo <nicvogue@gmail.com>
2023-03-06 16:27:43 -0800

    Escape apos on aboutme

--- components/homepage/AboutMe/AboutMe.js
+++ components/homepage/AboutMe/AboutMe.js
@@ -7,7 +7,7 @@ const AboutMe = () => {
 				Hello there! 👋
 			</h3>
 			<p className={aboutLook.description}>
-				My name is Nicolas, and I'm a freelance front-end developer based in San Diego, California.
+				My name is Nicolas, and I&apos;m a freelance front-end developer based in San Diego, California.
 			</p>
 			<p className={aboutLook.description}>
 				I design solutions with minimialist aesthetic and maximum function for a seamless, comprehensive user experience. Check out my work below!
