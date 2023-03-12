5a0c09681b93a0f0d6102bb4bd58ebbd923bc129
Nicolas Vo <nicvogue@gmail.com>
2023-03-12 01:28:16 -0800

    Move info in homepage project cards to container
    Makes transitions easily; one large parent instead of per element transitions
    Simplify background image filter
    More responsive

--- components/homepage/ProjectCard/ProjectCard.js
+++ components/homepage/ProjectCard/ProjectCard.js
@@ -1,21 +1,41 @@
 import Link from 'next/link';
 import { FaArrowRight } from 'react-icons/fa';
-
 import { TechBubble } from '../../global';
 
 import cardLook from './ProjectCard.module.scss';
 
-const ProjectCard = ({ categoryName, project, title, techs, wip }) => {
+const ProjectCard = ({
+	categoryName,
+	project,
+	title,
+	techs,
+	wip }) => {
+	const titleCased = title.replace(/([a-z])([A-Z])/g, '$1 $2');
 	return (
 		<li className={cardLook.cardContainer}>
-			<h4 className={cardLook.projectTitle}>{title.replace(/([a-z])([A-Z])/g, '$1 $2')}</h4>
-			<ul className={cardLook.techList}>
-				{techs.map((tech, index) => {
-					return <TechBubble key={`${categoryName}-${project}-${tech}`} tech={tech} styler={{ transitionDelay: `${index * 50}ms` }} />
-				})}
-			</ul>
-			{wip === false && <Link href={`${categoryName}/${project}`} className={cardLook.link}>Check it out<FaArrowRight /></Link>}
-			<img src={`thumbs/${categoryName}/${project}.png`} className={cardLook.imgBack} alt=''/>
+			<div className={cardLook.infoContainer}>
+				<h4 className={cardLook.projectTitle}>{titleCased}</h4>
+				<ul className={cardLook.techList}>
+					{
+						techs.map((tech, index) => {
+							return <TechBubble
+								key={`${categoryName}-${project}-${tech}`}
+								tech={tech}
+								styler={{ transitionDelay: `${index * 50}ms` }} />
+						})
+					}
+				</ul>
+				{
+					wip === false &&
+					<Link href={`${categoryName}/${project}`} className={cardLook.link}>
+						Check it out<FaArrowRight />
+					</Link>
+				}
+			</div>
+			<img
+				src={`thumbs/${categoryName}/${project}.png`}
+				className={cardLook.imgBack}
+				alt='' />
 		</li>
 	)
 };
