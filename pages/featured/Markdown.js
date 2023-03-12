ea354da932bf3bdf499e2119016ae686e3a30f0b
Nicolas Vo <nicvogue@gmail.com>
2023-03-12 00:59:26 -0800

    Move Markdown <Head> to projectLayout
    Also prettify

--- pages/featured/Markdown.js
+++ pages/featured/Markdown.js
@@ -1,46 +1,40 @@
-import Head from 'next/head';
+import { ProjectLayout } from '../../components/global';
+import { Markdown } from '../../components/fcc';
+
 import { getProjectLists, getProjectData } from '../../lib/props/homepage/projects';
 import { getVersionNumber } from '../../lib/props/homepage/homepage';
-import { Markdown } from '../../components/fcc';
-import { ProjectLayout } from '../../components/global';
 
 const MarkdownPage = ({ layoutData, projectData }) => {
-
-	const { title, slugline } = projectData;
-
 	return (
-		<>
-			<Head>
-				<title>{title}</title>
-				<meta name='description' content={slugline} />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-
-			<ProjectLayout layoutData={layoutData} projectData={projectData}>
-				<Markdown />
-			</ProjectLayout>
-		</>
+		<ProjectLayout layoutData={layoutData} projectData={projectData}>
+			<Markdown />
+		</ProjectLayout>
 	);
 };
 
 export default MarkdownPage;
 
 export async function getStaticProps() {
-	const layoutFetch = await Promise.all([await getProjectLists({ dataTypes: ['title'] }), await getVersionNumber(), await getProjectData({
-		category: 'featured',
-		project: 'Markdown',
-		types: ['title',
-			'slugline',
-			'description',
-			'techs',
-			'original']
-	})]);
+	const layoutFetch = await Promise.all([
+		await getProjectLists({ dataTypes: ['title'] }),
+		await getVersionNumber(),
+		await getProjectData({
+			category: 'featured',
+			project: 'Markdown',
+			types: ['title',
+				'slugline',
+				'description',
+				'techs',
+				'original']
+		})]);
+
 	const layoutData = {
 		otherProjects: layoutFetch[0],
 		version: layoutFetch[1],
 		linkExclude: 'Markdown'
 	};
 	const projectData = layoutFetch[2];
+
 	return {
 		props: {
 			layoutData,
