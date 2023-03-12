5bfb92164114c8c993867c6ac3efd86173f22614
Nicolas Vo <nicvogue@gmail.com>
2023-03-12 00:46:30 -0800

    Prettify BBGC photo asset objs

--- components/professional/BlockBuildersGC/assets/index.js
+++ components/professional/BlockBuildersGC/assets/index.js
@@ -4,11 +4,30 @@ import phome from './phome.png';
 import pinterior from './pinterior.png';
 import pinteriorpreview from './pinteriorpreview.png';
 
-const contactObj = { photo: contact, desc: 'BBGC contact form' };
-const homeObj = { photo: home, desc: 'BBGC homepage' };
-const phomeObj = { photo: phome, desc: 'BBGC portfolio landing page' };
-const pinteriorObj = { photo: pinterior, desc: 'BBGC portfolio interior category' };
-const pinteriorpreviewObj = { photo: pinteriorpreview, desc: 'BBGC portfolio interior example preview' };
+const contactObj = {
+	photo: contact,
+	desc: 'BBGC contact form'
+};
+
+const homeObj = {
+	photo: home,
+	desc: 'BBGC homepage'
+};
+
+const phomeObj = {
+	photo: phome,
+	desc: 'BBGC portfolio landing page'
+};
+
+const pinteriorObj = {
+	photo: pinterior,
+	desc: 'BBGC portfolio interior category'
+};
+
+const pinteriorpreviewObj = {
+	photo: pinteriorpreview,
+	desc: 'BBGC portfolio interior example preview'
+};
 
 export {
 	contactObj,
