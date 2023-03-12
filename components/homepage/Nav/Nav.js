116a32d6328d4d60d5a7f890cdfa19036d8edaec
Nicolas Vo <nicvogue@gmail.com>
2023-03-12 01:29:17 -0800

    Code cleanup homepage Nav

--- components/homepage/Nav/Nav.js
+++ components/homepage/Nav/Nav.js
@@ -8,26 +8,36 @@ const Nav = ({ projectList }) => {
 		<section className={navLook.container} id='projectNav'>
 			<h2 className={homeLook.hTwo}>Projects</h2>
 			<nav className={navLook.nav}>
-				{projectList.map((category) => {
-					const { categoryName, projects } = category;
-					return (
-						<section key={`${category.categoryName}-links`} className={navLook.catSection}>
-							<h3 className={navLook.catHeading}>{categoryName === 'fcc' ? 'freeCodeCamp' : categoryName[0].toUpperCase() + categoryName.slice(1)}</h3>
-							{projects.length !== 0 ?
-								<ul className={navLook.list}>
-									{
-										projects.map((projectObj) => {
-											const { title, techs, project, wip } = projectObj;
-											return (
-												<ProjectCard categoryName={categoryName} title={title} project={project} techs={techs} wip={wip} key={`${categoryName}-${title}`} />
-											)
-										})
-									}
-								</ul> : <em>There&apos;s nothing here...yet</em>
-							}
-						</section>
-					)
-				})}
+				{
+					projectList.map((category) => {
+						const { categoryName, projects } = category;
+						return (
+							<section key={`${category.categoryName}-links`} className={navLook.catSection}>
+								<h3 className={navLook.catHeading}>
+									{categoryName[0].toUpperCase() + categoryName.slice(1)}
+								</h3>
+								{
+									<ul className={navLook.list}>
+										{
+											projects.map((projectObj) => {
+												const { title, techs, project, wip } = projectObj;
+												return (
+													<ProjectCard
+														categoryName={categoryName}
+														title={title}
+														project={project}
+														techs={techs}
+														wip={wip}
+														key={`${categoryName}-${title}`} />
+												);
+											})
+										}
+									</ul>
+								}
+							</section>
+						);
+					})
+				}
 			</nav>
 		</section>
 	);
