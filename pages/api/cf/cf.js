c8e31b196d92d1d4a0399da16783215a2021018f
Nicolas Vo <nicvogue@gmail.com>
2023-03-12 01:05:16 -0800

    Condense contact form ReCAPTCHA fail conds
    Two cause the same 404
    Various prettier and comments

--- pages/api/cf/cf.js
+++ pages/api/cf/cf.js
@@ -1,27 +1,33 @@
 import { htmlStringer, plaintextStringer, validator } from '../../../lib/api/homepage/ContactForm';
 
 const formHandler = async (req, res) => {
-	const { name, email, threeToken, birthday } = req.body;
+	const { body } = req;
 
 	// Data validation
 	if (validator(body) === false) {
 		return res.status(404).json({ message: 'Not found' });
 	};
 
-	const rResponse = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.PRIVATE_CONTACT_FORM_RECAPTCHA_KEY}&response=${threeToken}`);
-	const captchaData = await rResponse.json();
-	if (captchaData.score < 0.3) {
+	// ReCAPTCHA validation
+	const { threeToken } = body;
+	const ReCAPTCHAResponse = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.PRIVATE_CONTACT_FORM_RECAPTCHA_KEY}&response=${threeToken}`);
+	const ReCAPTCHAData = await ReCAPTCHAResponse.json();
+	// Only valid error to report to user is if they took too long
+	if (ReCAPTCHAData['error-codes'] !== undefined &&
+		ReCAPTCHAData['error-codes'].includes('timeout-or-duplicate') === true) {
+		return res.status(502)
+			.json({ message: 'ReCAPTCHA response expired. Please submit again.' });
+	};
+	// If request fails at network level?
+	// Arbritrary score indicaitng bot?
+	if (ReCAPTCHAData.score < 0.3 || ReCAPTCHAData.success === false) {
 		return res.status(404).json({ message: 'Not found' });
 	};
 
-	if (captchaData.success === false) {
-		if (captchaData['error-codes'].includes('timeout-or-duplicate') === true) {
-			return res.status(502).json({ message: 'The ReCAPTCHA token expired. Please submit your information again.' })
-		};
-		return res.status(404).json({ message: 'Not found' });
-	};
-
+	// Emailer
+	const { name, email } = body;
 	const mailer = require('nodemailer');
+	// Some gmail docs indicate the correct smtp settings / procedure for handshakes
 	const transport = mailer.createTransport({
 		host: 'smtp.gmail.com',
 		port: 587,
@@ -32,13 +38,13 @@ const formHandler = async (req, res) => {
 			pass: process.env.PRIVATE_EMAIL_PASS
 		}
 	});
-
+	// Unknown if this works
 	transport.verify((error, success) => {
 		if (error) {
-			return res.status(502).json({ message: 'We had an internal server error, but your information is okay. Please try again in a few minutes.' })
-		}
+			return res.status(502)
+				.json({ message: 'Server error. Submit your info again.' });
+		};
 	});
-
 	// Send both plaintext and HTML; HTML from stringer function
 	const emailMessage = {
 		from: process.env.PRIVATE_EMAIL_USER,
@@ -47,14 +53,13 @@ const formHandler = async (req, res) => {
 		text: plaintextStringer(name),
 		html: htmlStringer(name)
 	};
-
 	const sendResult = await transport.sendMail(emailMessage);
 	if (sendResult.err) {
-		return res.status(502).json({ message: 'We had an internal server error, but your information is okay. Please try again in a few minutes.' });
-
+		return res.status(502)
+			.json({ message: 'Server error. Submit your info again.' });
 	};
-
-	return res.status(200).json({ message: `Information received. You should receive an email confirmation shortly.` });
+	return res.status(200)
+		.json({ message: `Received. An email confirmation should arrive soon.` });
 };
 
 export default formHandler;
