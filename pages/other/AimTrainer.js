237e49ef773a9b1118cb0e29adea125179a2fc77
Nicolas Vo <nicvogue@gmail.com>
2023-03-12 00:56:12 -0800

    Move AimTrainer <Head> to projectLayout
    Also prettify

--- pages/other/AimTrainer.js
+++ pages/other/AimTrainer.js
@@ -1,43 +1,43 @@
-import Head from 'next/head';
-import { getProjectLists, getProjectData } from '../../lib/props/homepage/projects';
-import { getVersionNumber } from '../../lib/props/homepage/homepage';
 import { ProjectLayout } from '../../components/global';
 import { AimTrainer } from '../../components/personal';
 import { Wip } from '../../components/global';
 
+import { getProjectLists, getProjectData } from '../../lib/props/homepage/projects';
+import { getVersionNumber } from '../../lib/props/homepage/homepage';
+
+
 const AimTrainerPage = ({ layoutData, projectData }) => {
-	const { title, slugline } = projectData;
-	return (<>
-		<Head>
-			<title>{title}</title>
-			<meta name='description' content={slugline} />
-			<link rel='icon' href='/favicon.ico' />
-		</Head>
+	return (
 		<ProjectLayout layoutData={layoutData} projectData={projectData}>
 			{/* <AimTrainer /> */}
 			<Wip />
 		</ProjectLayout>
-	</>);
+	);
 };
 
 export default AimTrainerPage;
 
 export async function getStaticProps() {
-	const layoutFetch = await Promise.all([await getProjectLists({ dataTypes: ['title'] }), await getVersionNumber(), await getProjectData({
-		category: 'other',
-		project: 'AimTrainer',
-		types: ['title',
-			'slugline',
-			'description',
-			'techs',
-			'original']
-	})]);
+	const layoutFetch = await Promise.all([
+		await getProjectLists({ dataTypes: ['title'] }),
+		await getVersionNumber(),
+		await getProjectData({
+			category: 'other',
+			project: 'AimTrainer',
+			types: ['title',
+				'slugline',
+				'description',
+				'techs',
+				'original']
+		})]);
+
 	const layoutData = {
 		otherProjects: layoutFetch[0],
 		version: layoutFetch[1],
 		linkExclude: 'AimTrainer'
 	};
 	const projectData = layoutFetch[2];
+
 	return {
 		props: {
 			layoutData,
