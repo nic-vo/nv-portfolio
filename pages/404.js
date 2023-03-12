b3b38dd64170a5670c3b845e18f7e631e1f7e790
Nicolas Vo <nicvogue@gmail.com>
2023-03-12 00:49:41 -0800

    Prettify 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Head from 'next/head';
 import { Duckies } from '../components/homepage';
 import { FaArrowLeft } from 'react-icons/fa';
 
@@ -7,12 +8,21 @@ import homeLook from '../components/homepage/Homepage.module.scss';
 
 const custom404 = () => {
 	return (
-		<main className={fourLook.container}>
-			<h1 className={homeLook.hTwo}>404: Not Found.</h1>
-			<Link href='/'><FaArrowLeft /><p>Back to home</p></Link>
-			<p className={fourLook.blurb}>You didn&apos;t find what you were looking for, but you found the ducks. Say hi.</p>
-			<Duckies />
-		</main>
+		<>
+			<Head>
+				<title>404 - Oops!</title>
+			</Head>
+
+			<main className={fourLook.container}>
+				<h1 className={homeLook.hTwo}>404: Not Found.</h1>
+				<Link href='/'><FaArrowLeft /><p>Back to home</p></Link>
+				<p className={fourLook.blurb}>
+					You didn&apos;t find what you were looking for,
+					but you found the ducks. Say hi.
+				</p>
+				<Duckies />
+			</main>
+		</>
 	);
 };
 
