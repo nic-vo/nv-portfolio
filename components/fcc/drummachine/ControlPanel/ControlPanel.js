b4a211329e4bbd04402897205bb3874f7eecd245
Nicolas Vo <nicvogue@gmail.com>
2023-03-12 01:23:38 -0800

    DrumMachine component code clean up

--- components/fcc/drummachine/ControlPanel/ControlPanel.js
+++ components/fcc/drummachine/ControlPanel/ControlPanel.js
@@ -6,11 +6,18 @@ import controlStyles from './ControlPanel.module.scss';
 
 
 const MasterSlider = ({ mVolume, mVolumeHandler }) => {
-
 	return (
 		<label htmlFor='mVolume'>
 			<FaVolumeUp />
-			<input id='mVolume' type='range' min='0' max='1' step='0.05' value={mVolume} onChange={mVolumeHandler} onPointerUp={mVolumeHandler} />
+			<input
+				id='mVolume'
+				type='range'
+				min='0'
+				max='1'
+				step='0.05'
+				value={mVolume}
+				onChange={mVolumeHandler}
+				onPointerUp={mVolumeHandler} />
 		</label>
 	);
 };
@@ -38,12 +45,30 @@ const ControlPanel = ({
 			</div>
 			<div className={controlStyles.controls}>
 				<div className={controlStyles.sliders}>
-					<label htmlFor='bank'><FaMusic /><input id='bank' type='range' min='0' max={numberOfBanks} step='1' value={bankIndex} onChange={bankIndexHandler} /></label>
+					<label htmlFor='bank'>
+						<FaMusic />
+						<input
+							id='bank'
+							type='range'
+							min='0'
+							max={numberOfBanks}
+							step='1'
+							value={bankIndex}
+							onChange={bankIndexHandler} />
+					</label>
 					<MasterSlider mVolume={mVolume} mVolumeHandler={mVolumeHandler} />
 				</div>
 				<div className={controlStyles.buttons}>
-					<button className={`${machineStyles.button} ${machineStyles.stop}`} onClick={stopAllHandler}><FaStop /></button>
-					<button className={`${machineStyles.button} ${muteAll ? machineStyles.muteon : machineStyles.mute}`} onClick={muteAllHandler}>{muteAll === true ? <FaVolumeMute /> : <FaVolumeOff />}</button>
+					<button
+						className={`${machineStyles.button} ${machineStyles.stop}`}
+						onClick={stopAllHandler}>
+						<FaStop />
+					</button>
+					<button
+						className={`${machineStyles.button} ${muteAll ? machineStyles.muteon : machineStyles.mute}`}
+						onClick={muteAllHandler}>
+						{muteAll === true ? <FaVolumeMute /> : <FaVolumeOff />}
+					</button>
 				</div>
 			</div>
 		</div>
