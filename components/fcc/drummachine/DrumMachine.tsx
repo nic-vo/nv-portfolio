8a0af6546c22babc82640d9d22c9afe46612ee21
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2023-10-16 16:47:54 -0700

    Convert DrumMachine to TypeScript
    Only the parent: enforces the prop types accepted from the prop getter

--- components/fcc/drummachine/DrumMachine.tsx
+++ components/fcc/drummachine/DrumMachine.tsx
@@ -1,16 +1,18 @@
-import { useState, useCallback, useMemo } from 'react';
+import { useState, useCallback, useMemo, KeyboardEventHandler } from 'react';
 import Pad from './Pad/Pad';
 import ControlPanel from './ControlPanel/ControlPanel';
 
 import machineStyles from './DrumMachine.module.scss';
+import { DrumMachineProps } from '@lib/props/types/projects';
 
 
-const DrumMachine = ({ assetInfo }) => {
-	const banks = useMemo(() => { return assetInfo.banks }, []);
-	const numberOfBanks = useMemo(() => { return assetInfo.numberOfBanks }, []);
-	const soundList = useMemo(() => { return assetInfo.soundList }, []);
+const DrumMachine = (props: { assetInfo: DrumMachineProps }) => {
+	const { assetInfo } = props;
+	const banks = useMemo(() => assetInfo.banks, []);
+	const numberOfBanks = useMemo(() => assetInfo.numberOfBanks, []);
+	const soundList = useMemo(() => assetInfo.soundList, []);
 
-	const chars = useMemo(() => { return ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c'] }, []);
+	const chars = useMemo(() => ['q', 'w', 'e', 'a', 's', 'd', 'z', 'x', 'c'], []);
 
 	// Keeps track of sound last played, active sound bank, master volume
 	const [displaySound, setDisplaySound] = useState('');
@@ -18,46 +20,45 @@ const DrumMachine = ({ assetInfo }) => {
 	const [mVolume, setMVolume] = useState(0.5);
 	const [muteAll, setMuteAll] = useState(false);
 
-	const setDisplaySoundHandler = (sound) => {
+	const setDisplaySoundHandler = (sound: string) => {
 		setDisplaySound(sound);
-	};
+	}
 
-	const clearDisplaySoundHandler = (sound) => {
-		if (sound === displaySound) {
-			setDisplaySound('');
-		};
-	};
+	const clearDisplaySoundHandler = (sound: string) => {
+		if (sound === displaySound) setDisplaySound('');
+	}
 
-	const mVolumeHandler = (e) => {
-		setMVolume(parseFloat(e.target.value));
-	};
+	const mVolumeHandler = (value: string) => {
+		setMVolume(parseFloat(value));
+	}
 
-	const bankIndexHandler = (e) => {
-		setBankIndex(parseInt(e.target.value));
+	const bankIndexHandler = (value: string) => {
+		setBankIndex(parseInt(value));
 		setDisplaySound('');
-	};
+	}
 
 	// Triggers sounds
-	const keyDownHandler = (e) => {
+	const keyDownHandler: KeyboardEventHandler = (e) => {
 		e.preventDefault();
 		if (chars.includes(e.key)) {
-			const player = document.getElementById(e.key);
+			const player = document.getElementById(e.key) as HTMLAudioElement;
+			if (player === null) return null;
 			player.pause();
 			player.currentTime = 0;
 			player.play();
 		} else if (e.key === ' ') {
 			stopAllHandler();
-		};
-	};
+		}
+	}
 
 	// Iterate through all audio elements and stop them
 	const stopAllHandler = useCallback(() => {
 		const audios = document.getElementsByTagName('audio');
 		for (let i = 0; i < audios.length; i++) {
-			const active = audios.item(i);
+			const active = audios.item(i) as HTMLAudioElement;
 			active.pause();
 			active.currentTime = 0;
-		};
+		}
 	}, []);
 
 	const muteAllHandler = useCallback(() => {
@@ -69,7 +70,7 @@ const DrumMachine = ({ assetInfo }) => {
 	return (
 		<section
 			className={machineStyles.machine}
-			tabIndex='0'
+			tabIndex={0}
 			onKeyDown={keyDownHandler}>
 			<ControlPanel
 				banks={banks}
