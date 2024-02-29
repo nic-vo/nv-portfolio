3a0c0ac0c1e963752761bcfd6377df47b6af346b
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-02-28 19:03:29 -0800

    Be rid of dynamic loader for image carousel projects

--- components/professional/index.ts
+++ components/professional/index.ts
@@ -1,25 +1,29 @@
-import dynamic from 'next/dynamic';
-import ProjectLoader from '@components/global/layouts/ProjectLayout/ProjectLoader/ProjectLoader';
+import BlockBuildersGC from './BlockBuildersGC/BlockBuildersGC';
+import PortfolioSite from './PortfolioSite/PortfolioSite';
+import LofiCafe from './LofiCafe/LofiCafe';
 
-const BlockBuildersGC = dynamic(() => import('./BlockBuildersGC/BlockBuildersGC'),
-	{
-		ssr: false,
-		loading: ProjectLoader
-	}
-);
-const PortfolioSite = dynamic(() => import('./PortfolioSite/PortfolioSite'),
-	{
-		ssr: false,
-		loading: ProjectLoader
-	}
-);
+// import dynamic from 'next/dynamic';
+// import ProjectLoader from '@components/global/layouts/ProjectLayout/ProjectLoader/ProjectLoader';
 
-const LofiCafe = dynamic(() => import('./LofiCafe/LofiCafe'),
-	{
-		ssr: false,
-		loading: ProjectLoader
-	}
-);
+// const BlockBuildersGC = dynamic(() => import('./BlockBuildersGC/BlockBuildersGC'),
+// 	{
+// 		ssr: false,
+// 		loading: ProjectLoader
+// 	}
+// );
+// const PortfolioSite = dynamic(() => import('./PortfolioSite/PortfolioSite'),
+// 	{
+// 		ssr: false,
+// 		loading: ProjectLoader
+// 	}
+// );
+
+// const LofiCafe = dynamic(() => import('./LofiCafe/LofiCafe'),
+// 	{
+// 		ssr: false,
+// 		loading: ProjectLoader
+// 	}
+// );
 
 export {
 	BlockBuildersGC,
