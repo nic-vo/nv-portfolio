ed16b7df147385d8305113102f5d009b87a77d6c
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-23 16:23:55 -0700

    Move the description components to parallel route

---
+++ src/app/(projects)/default.tsx
@@ -0,0 +1,7 @@
+const ProjectPageDefault = () => {
+	return (
+		<div className='h-full w-full flex items-center'>
+			<p>The the description for this page hasn&apos;t been implemented yet</p>
+		</div>
+	);
+};
