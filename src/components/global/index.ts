be755937944e8983501aee282deabd93170b660f
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-23 16:57:22 -0700

    Move ImageCarousel to page components

--- src/components/global/index.ts
+++ src/components/global/index.ts
@@ -1,8 +1,5 @@
 import Spinner from './misc/Spinner/Spinner';
-import Footer from '../../app/_components_/Footer/Footer';
 import Wip from './misc/Wip/Wip';
-import ImageCarousel from './misc/ImageCarousel/ImageCarousel';
-import TechBubble from './misc/TechBubble/TechBubble';
 import ProjectCard from './misc/ProjectCard/ProjectCard';
 
-export { Spinner, Footer, Wip, ImageCarousel, TechBubble, ProjectCard };
+export { Spinner, Wip, ProjectCard };
