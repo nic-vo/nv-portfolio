224ef17c2fc038d2975f868a11187668dda6a1c2
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-23 16:32:14 -0700

    Handle new prop format
    Handle tabIndex at parent wherever possible
    To tailwind too

--- src/components/global/layouts/ProjectLayout/Header/ProjectCategoryNavItem/ProjectCategoryNavItem.tsx
+++ src/components/global/layouts/ProjectLayout/Header/ProjectCategoryNavItem/ProjectCategoryNavItem.tsx
@@ -1,23 +1,26 @@
+'use cilent';
+
 import { useState } from 'react';
 import { FaCaretDown } from 'react-icons/fa';
-
-import pCatNavLook from './ProjectCategoryNavItem.module.scss';
 import Link from 'next/link';
 
+import pCatNavLook from './ProjectCategoryNavItem.module.scss';
+import { latoClass } from '@/styles/fonts';
+
 const ProjectCategoryNavItem = (props: {
-	category: {
-		categoryName: string,
-		projects: {
-			project: string,
-			title: string
-		}[]
-	},
-	dev: boolean
+	paths: {
+		root: string;
+		pages: {
+			segment: string;
+			title: string;
+		}[];
+	};
+	dev?: boolean;
 }) => {
+	const { root, pages } = props.paths;
 	const [catToggled, setCatToggled] = useState(false);
 
-	const { categoryName, projects } = props.category;
-	const catCased = categoryName[0].toUpperCase() + categoryName.slice(1).toLowerCase();
+	const catCased = root[0].toUpperCase() + root.slice(1).toLowerCase();
 
 	const clickToggleListHandler = () => {
 		setCatToggled(!catToggled);
@@ -26,64 +29,74 @@ const ProjectCategoryNavItem = (props: {
 	const classer = catToggled === true ? ` ${pCatNavLook.toggled}` : '';
 
 	return (
-		<li className={pCatNavLook.liContainer}>
-			{
-				props.dev === true ?
-					<button
-						id='hndev'
-						onClick={clickToggleListHandler}
-						className={pCatNavLook.toggler}
-						aria-label={`Toggle navigation submenu for ${catCased}`}
-						aria-pressed={catToggled}>
-						{catCased}
-						<FaCaretDown
-							className={pCatNavLook.svg + classer}
-							aria-hidden='true'
-							role='presentation' />
-					</button>
-					:
-					<button
-						onClick={clickToggleListHandler}
-						className={pCatNavLook.toggler}
-						aria-label={`Toggle navigation submenu for ${catCased}`}
-						aria-pressed={catToggled}>
-						{catCased}
-						<FaCaretDown
-							className={pCatNavLook.svg + classer}
-							aria-hidden='true'
-							role='presentation' />
-					</button>
-			}
-			<ul className={pCatNavLook.inCatList + classer}>
-				{
-					projects.map((project, index) => {
-						if (index === 0 && props.dev === true) return (
+		<li className='w-full'>
+			{props.dev === true ? (
+				<button
+					id='hndev'
+					onClick={clickToggleListHandler}
+					className={
+						latoClass +
+						' cursor-pointer flex justify-between items-center h-16 w-full p-4 text-xl font-bold bg-inherit text-inherit border-0 border-black border-b'
+					}
+					aria-label={`Toggle navigation submenu for ${catCased}`}
+					aria-pressed={catToggled}>
+					{catCased}
+					<FaCaretDown
+						className={pCatNavLook.svg + classer}
+						aria-hidden='true'
+						role='presentation'
+					/>
+				</button>
+			) : (
+				<button
+					onClick={clickToggleListHandler}
+					className={
+						latoClass +
+						' cursor-pointer flex justify-between items-center h-16 w-full p-4 text-xl font-bold bg-inherit text-inherit border-0 border-black border-b'
+					}
+					aria-label={`Toggle navigation submenu for ${catCased}`}
+					aria-pressed={catToggled}>
+					{catCased}
+					<FaCaretDown
+						className={pCatNavLook.svg + classer}
+						aria-hidden='true'
+						role='presentation'
+					/>
+				</button>
+			)}
+			<ul
+				className={pCatNavLook.inCatList + classer}
+				tabIndex={catToggled ? 0 : -1}>
+				{pages.map((page, index) => {
+					if (index === 0 && props.dev === true)
+						return (
 							<li
 								id={props.dev === true && index === 0 ? 'hnlidev' : ''}
 								className={pCatNavLook.li + classer}
-								key={`${categoryName}-list-${project.project}`}
-								tabIndex={-1}>
-								<Link href={`/${categoryName}/${project.project}`} prefetch={false}>
-									{project.title}
+								key={`${root}-list-${page.segment}`}>
+								<Link
+									href={`/${root}/${page.segment}`}
+									prefetch={false}>
+									{page.title}
 								</Link>
 							</li>
 						);
-						else
-							return (
-								<li
-									className={pCatNavLook.li + classer}
-									key={`${categoryName}-list-${project.project}`}
-									tabIndex={-1}	>
-									<Link href={`/${categoryName}/${project.project}`} prefetch={false}>
-										{project.title}
-									</Link>
-								</li>
-							);
-					})
-				}
+					else
+						return (
+							<li
+								className={pCatNavLook.li + classer}
+								key={`${root}-list-${page.segment}`}>
+								<Link
+									href={`/${root}/${page.segment}`}
+									prefetch={false}>
+									{page.title}
+								</Link>
+							</li>
+						);
+				})}
 			</ul>
 		</li>
 	);
-}
+};
 
 export default ProjectCategoryNavItem;
