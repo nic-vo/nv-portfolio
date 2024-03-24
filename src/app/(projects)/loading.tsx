f12fc4a4f563adbb83d18fa73071d15084d8cfec
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-23 17:48:34 -0700

    Delete layout stuff / move to project layout comps

---
+++ src/app/(projects)/loading.tsx
@@ -0,0 +1,9 @@
+const ProjectPageLoading = () => {
+	return (
+		<div className='flex justify-center items-center text-3xl'>
+			<p className='m-0 p-0'>Project loading...</p>;
+		</div>
+	);
+};
+
+export default ProjectPageLoading;
--- src/components/global/layouts/index.ts
+++
@@ -1,9 +0,0 @@
-import ProjectLayout from './ProjectLayout/ProjectLayout';
-import ProjectContainer from './ProjectLayout/ProjectContainer/ProjectContainer';
-import ProjectInfo from './ProjectLayout/ProjectInfo/ProjectInfo';
-
-export {
-	ProjectLayout,
-	ProjectInfo,
-	ProjectContainer
-};
