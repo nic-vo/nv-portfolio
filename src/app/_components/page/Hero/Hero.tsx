82619745c82fff304a6d64993f6f45eb7bfd5fbe
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-26 02:27:13 -0700

    Header tailwind adjustments

--- src/app/_components/page/Hero/Hero.tsx
+++ src/app/_components/page/Hero/Hero.tsx
@@ -4,11 +4,11 @@ import { poppinsClass, latoClass } from '@/styles/fonts';
 
 const Hero = () => {
 	return (
-		<header className='flex flex-col items-center justify-center w-full h-svh z-10'>
-			<h1 className={poppinsClass + ' text-[8vmax] font-bold m-0'}>
+		<header className='flex flex-col items-center justify-center w-full h-svh z-10 bg-[linear-gradient(black,transparent_80%)]'>
+			<h1 className={poppinsClass.className + ' text-9xl font-bold m-0'}>
 				Nicolas Vo
 			</h1>
-			<h2 className={latoClass + ' text-[4vmax] font-light m-0'}>
+			<h2 className={latoClass.className + ' text-6xl font-light m-0'}>
 				Front End Developer
 			</h2>
 			<Crosses limit={32} />
