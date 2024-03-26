341f52e60e2433e2709e70f752ef3aafea130f3c
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-26 02:28:52 -0700

    Redo home nav transitions purely in tailwind
    Order for its pseudoclass selectors is crazy

--- src/app/_components/page/Nav.tsx
+++ src/app/_components/page/Nav.tsx
@@ -15,19 +15,19 @@ const NavLink = async (props: {
 }) => {
 	const { title, segment } = props.page;
 	return (
-		<li className='w-[40svh] h-[calc(40svh*3/4)] bg-black rounded-3xl overflow-hidden list-none transition-all justify-self-center bg-cover shadow-none hover:shadow-white hover:shadow-[0px_0px_20px_5px]'>
+		<li className='w-[40svh] h-[calc(40svh*3/4)] list-none transition-all justify-self-center bg-cover '>
 			<Link
 				href={`${props.root}/${segment}`}
 				prefetch={false}
-				className='h-full w-full flex justify-center items-center bg-transparent text-white p-4 font-bold text-xl before:content-[""] before:block before:absolute before:bottom-0 before:h-1/2 before:w-full before:z-10 before:transition-all before:opacity-0 before:bg-[linear-gradient(transparent,_black)]hover:before:opacity-100 focus:before:opacity-100 hover:first:translate-y-[600%] focus:first:translate-y-[600%] lg:hover:first:translate-y-[400%] lg:focus:first:translate-y-[400%] hover:last:brightness-100'>
-				<span className='flex gap-2 transition-all z-20 drop-shadow-md border-b-2 border-white'>
+				className='overflow-hidden h-full w-full flex justify-center items-center bg-transparent rounded-3xl p-4 text-white font-bold text-xl shadow-none hover:shadow-white hover:shadow-[0px_0px_20px_5px] focus:shadow-white focus:shadow-[0px_0px_20px_5px] transition-all first:*:hover:translate-y-[350%] first:*:focus:translate-y-[350%] before:bg-[linear-gradient(transparent,rgba(0,0,0,0.6),rgba(0,0,0,0.8))] before:content-[""] before:transition-all before:z-10 before:h-1/3 before:w-full before:absolute before:bottom-0 before:opacity-0 before:hover:opacity-100 before:focus:opacity-100 last:*:brightness-50 last:*:hover:brightness-100 last:*:focus:brightness-100'>
+				<span className='flex gap-2 transition-all z-20 drop-shadow-md border-b-2 border-white bg-transparent'>
 					{title}
 				</span>
 				<Image
 					src={referenced[segment] ?? DefaultThumb}
 					alt={`Check out ${title}!`}
 					sizes='(max-aspect-ratio: 1) 100vw, 50vw'
-					className='absolute h-full w-auto brightness-50 transition-all'
+					className='absolute h-full w-auto transition-all'
 				/>
 			</Link>
 		</li>
@@ -41,14 +41,14 @@ const Nav = async () => {
 	]);
 	return (
 		<nav className='w-full py-48 px-16'>
-			<section className='flex flex-col items-center mb-48'>
-				<h2 className={latoClass + ' text-4xl text-center'}>
+			<section className='flex flex-col items-center mb-48 gap-8'>
+				<h2 className={latoClass.className + ' text-4xl text-center font-bold'}>
 					Featured Projects
 				</h2>
 				{featuredPaths.pages.length === 0 ? (
 					<p>Nothing yet...</p>
 				) : (
-					<ul className='grid-cols-1 lg:grid-cols-2 items-center justify-items-center w-fit gap-8 m-8 p-0'>
+					<ul className='grid grid-cols-1 lg:grid-cols-2 gap-12 p-0'>
 						{featuredPaths.pages.map((page) => (
 							<NavLink
 								key={page.segment}
@@ -60,11 +60,13 @@ const Nav = async () => {
 				)}
 			</section>
 			<section className='flex flex-col items-center mb-48'>
-				<h2 className={latoClass + ' text-4xl text-center'}>Other Projects</h2>
+				<h2 className={latoClass.className + ' text-4xl text-center font-bold'}>
+					Other Projects
+				</h2>
 				{otherPaths.pages.length === 0 ? (
 					<p>Nothing yet...</p>
 				) : (
-					<ul className='grid-cols-1 lg:grid-cols-2 items-center justify-items-center w-fit gap-8 m-8 p-0'>
+					<ul className='grid grid-cols-1 lg:grid-cols-2 gap-8 m-8 p-0'>
 						{otherPaths.pages.map((page) => (
 							<NavLink
 								root='other'
