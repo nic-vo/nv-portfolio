d59f97b97fc500cc6950586fb5f7f2ecdfc934ca
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-26 02:26:26 -0700

    Revamp projectinfo component
    Tailwind
    New props format
    Format in general

--- src/app/(projects)/_components/page/parts.tsx
+++ src/app/(projects)/_components/page/parts.tsx
@@ -5,8 +5,14 @@ import { parse } from 'marked';
 import { poppinsClass } from '@/styles/fonts';
 import look from './ProjectInfo.module.scss';
 
-export const getProjectDecsription = async (relativePath: string) => {
-	const mdPath = path.join(relativePath, 'description.md');
+export const getProjectDescription = async (relativePath: string) => {
+	const replaceWithSrc = relativePath.split('\\').slice(-4);
+	const mdPath = path.join(
+		process.cwd(),
+		'src',
+		...replaceWithSrc,
+		'description.md',
+	);
 	const buffer = await fs.readFile(mdPath, { encoding: 'utf-8' });
 	return await parse(buffer, { async: true });
 };
@@ -21,33 +27,39 @@ const ProjectInfo = (props: {
 
 	return (
 		<>
-			<h1 className={poppinsClass + ' text-6xl text-center m-4'}>{title}</h1>
-			<section className='flex flex-col gap-8 w-11/12 max-w-screen-lg pb-24'>
-				<section
-					className={look.description}
-					dangerouslySetInnerHTML={{ __html: description }}
-				/>
-				<section>
-					<h2>The stack:</h2>
-					<ul className='flex flex-wrap w-11/12 max-w-screen-lg p-0 z-10 text-xl'>
-						{techs.map((tech) => (
-							<li
-								key={tech}
-								className='flex items-center p-2 m-1 h-8 rounded-md font-bold text-base text-center text-white bg-sky-600'>
-								{tech}
-							</li>
-						))}
-					</ul>
-				</section>
-				<section className='flex items-center font-bold gap-4'>
-					<h2>Link to original:</h2>
-					<a
-						href={link}
-						target='_blank'
-						className='block px-8 py-4 rounded-3xl bg-green-500'>
-						Original / Live
-					</a>
-				</section>
+			<h1 className={poppinsClass.className + ' text-6xl text-center'}>
+				{title}
+			</h1>
+			<section className='flex gap-4'>
+				<h2 className={poppinsClass.className + ' text-3xl shrink-0'}>
+					The stack:
+				</h2>
+				<ul className='flex flex-wrap w-11/12 max-w-screen-lg p-0 z-10 text-xl'>
+					{techs.map((tech) => (
+						<li
+							key={tech}
+							className='flex items-center p-2 m-1 h-8 rounded-md font-bold text-base text-center text-white bg-sky-600'>
+							{tech}
+						</li>
+					))}
+				</ul>
+			</section>
+			<section
+				className={
+					look.description + ' max-w-prose border-t-2 border-white pb-8'
+				}
+				dangerouslySetInnerHTML={{ __html: description }}
+			/>
+			<section className='flex font-bold gap-4 w-full max-w-prose border-b-2 border-white pb-8'>
+				<h2 className={poppinsClass.className + ' text-3xl'}>
+					Link to original:
+				</h2>
+				<a
+					href={link}
+					target='_blank'
+					className='block px-4 py-2 rounded-3xl bg-green-500'>
+					Original / Live
+				</a>
 			</section>
 		</>
 	);
