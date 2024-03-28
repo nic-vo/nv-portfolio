c2cb08dced1c71e2f1d30c22f26dd7a803c400f3
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-27 18:03:52 -0700

    Init pages for featured interactive parts

---
+++ src/app/(projects)/@interactive/featured/MixDelta/page.tsx
@@ -0,0 +1,5 @@
+import MixDelta from './_components/MixDelta';
+
+export default MixDelta;
+
+export const metadata = { title: 'MixDelta - Spotify tool' };
---
+++ src/app/(projects)/@interactive/featured/PortfolioSite/page.tsx
@@ -0,0 +1,5 @@
+import PortfolioSite from './_components/PortfolioSite';
+
+export default PortfolioSite;
+
+export const metadata = { title: 'Portfolio Site v. 2.x' };
