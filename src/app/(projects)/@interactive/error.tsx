0edc23e50b4c9e25841723599593dbe9fd02812e
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-27 18:47:41 -0700

    Cleanup @interactive error page

--- src/app/(projects)/@interactive/error.tsx
+++ src/app/(projects)/@interactive/error.tsx
@@ -10,11 +10,19 @@ const ProjectInteractiveError = (props: {
 }) => {
 	return (
 		<>
-			<h2 className={poppinsClass.className + ' font-bold text-9xl'}>
+			<h2 className={poppinsClass.className + ' font-bold text-6xl'}>
 				Something went wrong!
 			</h2>
-			<Link href='/'>Go back home</Link>
-			<button onClick={props.reset}>Refresh?</button>
+			<Link
+				href='/'
+				className='block bg-transparent hover:bg-white focus:bg-white text-white hover:text-black focus:text-black p-4 font-bold text-2xl rounded-3xl border-2 border-white transition-all'>
+				Go back home
+			</Link>
+			<button
+				onClick={props.reset}
+				className='block bg-transparent hover:bg-white focus:bg-white text-white hover:text-black focus:text-black p-4 font-bold text-2xl rounded-3xl border-2 border-white transition-all'>
+				Refresh?
+			</button>
 		</>
 	);
 };
