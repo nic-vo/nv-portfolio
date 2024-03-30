dd0aabf745a7a4429cf1a284017b523590d9b149
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-30 16:55:08 -0700

    Add static crosses to provide reference point for animated ones

--- src/app/_components/page/Hero/Crosses.tsx
+++ src/app/_components/page/Hero/Crosses.tsx
@@ -1,57 +1,89 @@
 import { FaPlus } from 'react-icons/fa';
-import crossLook from './Crosses.module.scss';
+import { PropsWithChildren } from 'react';
 
-const calculator = () => {
-	// Distance from top and left
-	const y = Math.random() * 0.8;
-	const x = Math.random() * 0.8 + 0.1;
-	return { x, y };
-};
+import crossLook from './Crosses.module.scss';
 
 const Cross = (props: { coords: { x: number; y: number } }) => {
 	const rotate = (() => {
 		const sign = Math.random() > 0.5;
-		const deg = parseFloat(Math.random().toFixed(3)) * 10;
+		const deg = parseFloat(Math.random().toFixed(3)) * 15;
 		return sign ? 0 + deg : 0 - deg;
 	})();
+	const seed = parseFloat(Math.random().toFixed(3));
 	return (
 		<div
-			className={'absolute w-auto h-auto bg-transparent ' + crossLook.container}
+			className={'absolute w-auto h-auto bg-transparent ' + crossLook.animated}
 			style={{
 				left: `${props.coords.x * 100}%`,
-				top: `${props.coords.y * 100}%`,
-				animationDelay: `${parseFloat(Math.random().toFixed(2)) * 30 - 15}s`,
-				animationDuration: `${parseFloat(Math.random().toFixed(2)) * 30 + 30}s`,
+				top: `${props.coords.y * 80}%`,
+				animationDelay: `${seed * 60 - 30}s`,
+				animationDuration: `${seed * 30 + 30}s`,
 			}}>
-			<div
-				className='w-full h-full'
-				style={{ transform: `rotate(${rotate}deg)` }}>
-				<FaPlus />
-			</div>
+			<FaPlus
+				className='block text-2xl text-white'
+				style={{ transform: `rotate(${rotate}deg)` }}
+			/>
 		</div>
 	);
 };
 
-const Crosses = ({ limit }: { limit: number }) => {
-	const initer = (() => {
-		let arr = [];
-		for (let i = 0; i < limit; i++) arr.push(calculator());
-		return arr;
+const StaticCross = (props: { coords: { x: number; y: number } }) => {
+	const rotate = (() => {
+		const sign = Math.random() > 0.5;
+		const deg = parseFloat(Math.random().toFixed(3)) * 20;
+		return sign ? 0 + deg : 0 - deg;
 	})();
+
+	const distance = parseFloat(Math.random().toFixed(2));
+	const signedDistance = Math.random() > 0.5 ? 0 + distance : 0 - distance;
+	const seed = parseFloat(Math.random().toFixed(3));
 
 	return (
 		<div
-			className='absolute h-svh w-full overflow-hidden z-0'
-			style={{ perspective: '400px' }}>
+			className={'absolute w-auto h-auto bg-transparent ' + crossLook.static}
+			style={{
+				left: `${props.coords.x * 80 + 10}%`,
+				top: `${props.coords.y * 30 + 30}%`,
+				animationDelay: `${seed * 60 - 30}s`,
+				animationDuration: `${seed * 30 + 30}s`,
+				transform: `translateZ(${Math.floor(250 * signedDistance)}px)`,
+				filter: `blur(${Math.floor(distance * 5)}px) brightness(${1 - distance})`,
+			}}>
+			<FaPlus
+				className='block text-2xl text-white'
+				style={{ transform: `rotate(${rotate}deg)` }}
+			/>
+		</div>
+	);
+};
+
+const Crosses = async (
+	props: PropsWithChildren & {
+		pair: { x: number; y: number }[][];
+	},
+) => {
+	const [initer, staticIniter] = props.pair;
+
+	return (
+		<>
+			{staticIniter.map((coords, index) => {
+				return (
+					<StaticCross
+						coords={coords}
+						key={'static-' + index}
+					/>
+				);
+			})}
+			{props.children}
 			{initer.map((coords, index) => {
 				return (
 					<Cross
-						key={`cross-${index}`}
+						key={'animated-' + index}
 						coords={coords}
 					/>
 				);
 			})}
-		</div>
+		</>
 	);
 };
 
--- src/app/_components/page/Hero/Hero.tsx
+++ src/app/_components/page/Hero/Hero.tsx
@@ -1,17 +1,58 @@
 import Crosses from './Crosses';
 
 import { poppinsClass, latoClass } from '@/styles/fonts';
+import style from './Crosses.module.scss';
+import { unstable_cache } from 'next/cache';
 
-const Hero = () => {
+const generateCrosses = unstable_cache(
+	async (limit: number) => {
+		const calculator = () => {
+			// Distance from top and left
+			const y = Math.random() * 0.8;
+			const x = Math.random() * 0.8 + 0.1;
+			return { x, y };
+		};
+
+		const initer = (() => {
+			let arr = [];
+			for (let i = 0; i < limit; i++) arr.push(calculator());
+			return arr;
+		})();
+
+		const staticIniter = (() => {
+			let arr = [];
+			for (let i = 0; i < Math.floor(limit / 2); i++) arr.push(calculator());
+			return arr;
+		})();
+
+		return [initer, staticIniter];
+	},
+	['hero-particle-coords'],
+	{ revalidate: false },
+);
+
+const Hero = async () => {
+	const pair = await generateCrosses(20);
 	return (
-		<header className='flex flex-col items-center justify-center w-full h-svh z-10 bg-[linear-gradient(black,transparent_80%)]'>
-			<h1 className={poppinsClass.className + ' text-9xl font-bold m-0'}>
-				Nicolas Vo
-			</h1>
-			<h2 className={latoClass.className + ' text-6xl font-light m-0'}>
-				Front End Developer
-			</h2>
-			<Crosses limit={32} />
+		<header
+			className='flex flex-col items-center justify-center w-full h-svh z-10 bg-[linear-gradient(black,transparent_80%)]'
+			style={{ perspective: '400px' }}>
+			<Crosses pair={pair}>
+				<h1
+					className={
+						poppinsClass.className +
+						' text-9xl font-bold m-0 ' +
+						style.activate3d
+					}>
+					Nicolas Vo
+				</h1>
+				<h2
+					className={
+						latoClass.className + ' text-6xl font-light m-0 ' + style.activate3d
+					}>
+					Front End Developer
+				</h2>
+			</Crosses>
 		</header>
 	);
 };
