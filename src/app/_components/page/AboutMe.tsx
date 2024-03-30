05e03717ef01f3e21d0c24ea3523f1b73a1d4f87
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-03-30 16:48:51 -0700

    Re-style the AboutMe section

--- src/app/_components/page/AboutMe.tsx
+++ src/app/_components/page/AboutMe.tsx
@@ -1,20 +1,17 @@
-import aboutLook from './AboutMe.module.scss';
-import homeLook from '../Homepage.module.scss';
 import { poppinsClass } from '@/styles/fonts';
 
 const AboutMe = () => {
-	const twoClasser = [homeLook.hTwo, aboutLook.heading].join(' ');
 	return (
-		<section className='flex flex-col items-center w-full py-48 px-16'>
+		<section className='items-center p-48 grid grid-cols-4 gap-8'>
 			<h2
 				className={
-					poppinsClass +
-					' block p-4 bg-white text-black rounded-3xl max-w-screen-sm top-0 m-0 z-10 text-4xl text-center'
+					poppinsClass.className +
+					' block p-4 bg-white text-black max-w-screen-sm top-0 m-0 z-10 text-4xl text-center col-span-1'
 				}>
 				About Me
 			</h2>
-			<div className='bg-black bg-opacity-20 p-8 w-full max-w-screen-md rounded-3xl -translate-y-4 text-base font-light *:w-full lg:text-2xl'>
-				<p>Hello there! 👋</p>
+			<div className='col-span-3 flex flex-col gap-4 bg-black bg-opacity-20 p-8 border-l-2 border-white w-full max-w-prose font-light *:w-full text-lg'>
+				<p className='text-4xl'>Hello there! 👋</p>
 				<p>
 					My name is Nicolas, and I&apos;m a front end developer based in
 					southern California.
