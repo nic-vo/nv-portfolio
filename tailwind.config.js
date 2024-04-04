1ef06a2a07f323257af7f70c9e6c5df88f6cba6a
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-04-04 11:44:58 -0700

    Add fadein animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,15 @@ module.exports = {
 				white: '#D6E5E3',
 				black: '#0c1821',
 			},
+			keyframes: {
+				fadein: {
+					'0%': { opacity: 0 },
+					'100%': { opacity: 1 },
+				},
+			},
+			animation: {
+				fadein: 'fadein 0.5s ease 0s 1 normal both',
+			},
 		},
 	},
 	plugins: [],
