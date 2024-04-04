7452e72a7998743efada5762b003e867a340ec77
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-04-03 23:09:48 -0700

    Restructure About Me to be more semantic

--- src/app/_components/page/AboutMe.tsx
+++ src/app/_components/page/AboutMe.tsx
@@ -2,26 +2,25 @@ import { poppinsClass } from '@/styles/fonts';
 
 const AboutMe = () => {
 	return (
-		<section className='items-center py-48 w-11/12 lg:w-max px-8 flex flex-col justify-center lg:grid grid-cols-4 gap-8'>
+		<section className='py-48 w-11/12 lg:w-max px-8 flex flex-col justify-center lg:grid grid-cols-4 grid-rows-3 gap-8 lg:gap-y-0 lg:gap-x-8'>
 			<h2
 				className={
 					poppinsClass.className +
-					' block p-4 bg-white text-black top-0 m-0 z-10 text-4xl text-center col-span-1 rounded-3xl'
+					' block p-4 bg-white text-black top-0 m-0 z-10 text-4xl text-center col-span-1 row-span-3 rounded-3xl w-min text-nowrap self-center'
 				}>
 				About Me
 			</h2>
-			<div className='col-span-3 flex flex-col gap-4 bg-black bg-opacity-20 p-8 border-l-2 border-white w-full max-w-prose font-light *:w-full text-lg'>
-				<p className='text-2xl font-extrabold'>Hello there! 👋</p>
-				<p>
-					My name is Nicolas, and I&apos;m a front end developer based in
-					southern California.
-				</p>
-				<p>
-					I design solutions with a minimialist aesthetic and maximum function
-					for a seamless, comprehensive user experience. Check out my work
-					below!
-				</p>
-			</div>
+			<p className='text-2xl font-extrabold col-span-3 col-start-2 max-w-prose'>
+				Hello there! 👋
+			</p>
+			<p className='col-span-3 col-start-2 max-w-prose text-xl'>
+				My name is Nicolas, and I&apos;m a front end developer based in southern
+				California.
+			</p>
+			<p className='col-span-3 col-start-2 max-w-prose text-xl'>
+				I design solutions with a minimialist aesthetic and maximum function for
+				a seamless, comprehensive user experience. Check out my work below!
+			</p>
 		</section>
 	);
 };
