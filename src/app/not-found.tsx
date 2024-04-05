b05f2c2a1d1be6ab36f7135d911e06d560987add
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-04-05 02:58:52 -0700

    Replace fonts in global 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,15 +2,11 @@ import Link from 'next/link';
 import { Duckies } from './_components/page';
 import { FaArrowLeft } from 'react-icons/fa';
 
-import { poppinsClass } from '@/styles/fonts';
-
 const RootNotFound = () => {
 	return (
 		<>
 			<main className='h-lvh w-full flex flex-col items-center justify-center gap-8'>
-				<h1 className={poppinsClass.className + ' text-6xl font-bold m-0'}>
-					404: Not Found.
-				</h1>
+				<h1 className='font-poppins text-6xl font-bold m-0'>404: Not Found.</h1>
 				<Link
 					href='/'
 					className='flex gap-4 bg-white text-black p-4 rounded-full *:block items-center'>
