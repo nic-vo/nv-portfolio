8b7c819003bb2e0875ec3cf32b65c30c1d547aff
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-04-08 04:04:39 -0700

    Move layout fonts to root layout
    Add preload
    Lazy load ReCAPTCHA

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,42 +1,7 @@
-import {
-	Overpass,
-	Lato,
-	Poppins,
-	JetBrains_Mono,
-	Silkscreen,
-} from 'next/font/google';
-
-export const overpassClass = Overpass({
-	subsets: ['latin-ext'],
-	display: 'swap',
-	weight: 'variable',
-	variable: '--font-overpass',
-});
-
-export const poppinsClass = Poppins({
-	weight: ['700'],
-	subsets: ['latin-ext'],
-	display: 'swap',
-	variable: '--font-poppins',
-});
-
-export const latoClass = Lato({
-	weight: ['100', '300', '400', '700', '900'],
-	display: 'swap',
-	subsets: ['latin-ext'],
-	variable: '--font-lato',
-});
-
-export const jbMonoClass = JetBrains_Mono({
-	weight: 'variable',
-	display: 'swap',
-	subsets: ['latin-ext'],
-	variable: '--font-jbmono',
-});
+import { Silkscreen } from 'next/font/google';
 
 export const silkscreenClass = Silkscreen({
 	weight: '400',
-
 	subsets: ['latin'],
 	display: 'swap',
 	variable: '--font-silkscreen',
