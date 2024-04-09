677fce75f4ae469a544e984c164cd16deb2dc322
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-04-09 15:35:04 -0700

    Remove preload once again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ const overpassClass = Overpass({
 	subsets: ['latin-ext'],
 	display: 'swap',
 	variable: '--font-overpass',
-	preload: true,
+	preload: false,
 	fallback: ['serif', 'ui-serif', 'system-ui'],
 });
 
@@ -21,7 +21,7 @@ const poppinsClass = Poppins({
 	subsets: ['latin-ext'],
 	display: 'swap',
 	variable: '--font-poppins',
-	preload: true,
+	preload: false,
 	fallback: ['sans-serif', 'ui-sans-serif', 'system-ui'],
 });
 
@@ -30,7 +30,7 @@ const latoClass = Lato({
 	display: 'swap',
 	subsets: ['latin-ext'],
 	variable: '--font-lato',
-	preload: true,
+	preload: false,
 	fallback: ['sans-serif', 'ui-sans-serif', 'system-ui'],
 });
 
@@ -38,7 +38,7 @@ const jbMonoClass = JetBrains_Mono({
 	display: 'swap',
 	subsets: ['latin-ext'],
 	variable: '--font-jbmono',
-	preload: true,
+	preload: false,
 	fallback: ['monospace', 'ui-monospace', 'system-ui'],
 });
 
