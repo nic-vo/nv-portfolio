5b69c5183d9e18dd0c6b21252e24cdeaadb47aa9
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-05-16 19:55:28 -0700

    Fix nav cat item
    Improve / limit focus styling
    Wrap everything in a container
    Improve aria

--- src/app/(projects)/_components/layout/CategoryNavItem.tsx
+++ src/app/(projects)/_components/layout/CategoryNavItem.tsx
@@ -27,41 +27,43 @@ const CategoryNavItem = (props: {
 		<li
 			className='w-full bflag'
 			tabIndex={-1}>
-			<button
-				onClick={clickToggleListHandler}
-				className={`font-lato transition-colors cursor-pointer flex justify-between items-center h-16 w-full p-4 text-xl font-bold border-b border-black bflag ${catToggled ? 'bg-black text-white' : 'bg-white text-black'} z-10`}
-				aria-label={`${catToggled ? 'Close' : 'Open'} navigation submenu for ${catCased}`}
-				aria-pressed={catToggled && !parentToggled}
-				tabIndex={parentToggled ? 0 : -1}>
-				{catCased}
-				<IoCaretDown
-					className={`block text-2xl transition-all ${catToggled ? 'rotate-180' : ''}`}
-					aria-hidden='true'
-				/>
-			</button>
-			<menu
-				className={`${catToggled ? 'flex' : 'hidden'} flex-col relative m-0 p-0 list-none py-4 gap-4 z-0 border-b border-black bflag`}
-				tabIndex={-1}
-				aria-expanded={catToggled && !parentToggled}>
-				{pages.map((page) => {
-					const { segment, data } = page;
-					const { title } = data;
-					return (
-						<li
-							className='relative overflow-x-hidden w-full transition-all bflag'
-							key={`${root}-list-${segment}`}
-							tabIndex={-1}>
-							<Link
-								href={`/${root}/${segment}`}
-								prefetch={false}
-								tabIndex={catToggled && parentToggled ? 0 : -1}
-								className='flex items-center p-2 h-12 translate-x-4 text-black bg-white font-light transition-all focus:bg-black focus:text-white hover:bg-black hover:text-white focus:translate-x-0 hover:translate-x-0 rounded-md bflag'>
-								{title}
-							</Link>
-						</li>
-					);
-				})}
-			</menu>
+			<div>
+				<button
+					onClick={clickToggleListHandler}
+					className={`font-lato transition-colors cursor-pointer flex justify-between items-center h-16 w-full p-4 text-xl font-bold border-b border-black bflag ${catToggled ? 'bg-black text-white' : 'bg-white text-black'} z-10 outline-black focus-visible:outline`}
+					aria-label={`${catToggled ? 'Close' : 'Open'} navigation submenu for ${catCased}`}
+					aria-pressed={catToggled && !parentToggled}
+					tabIndex={parentToggled ? 0 : -1}>
+					{catCased}
+					<IoCaretDown
+						className={`block text-2xl transition-all ${catToggled ? 'rotate-180' : ''}`}
+						aria-hidden='true'
+					/>
+				</button>
+				<menu
+					className={`${catToggled ? 'flex' : 'hidden'} flex-col relative m-0 p-0 list-none py-4 gap-4 z-0 border-b border-black bflag`}
+					tabIndex={-1}
+					aria-expanded={catToggled && !parentToggled}>
+					{pages.map((page) => {
+						const { segment, data } = page;
+						const { title } = data;
+						return (
+							<li
+								className='relative overflow-x-hidden w-full transition-all bflag'
+								key={`${root}-list-${segment}`}
+								tabIndex={-1}>
+								<Link
+									href={`/${root}/${segment}`}
+									prefetch={false}
+									tabIndex={catToggled && parentToggled ? 0 : -1}
+									className='flex items-center p-2 h-12 translate-x-4 text-black bg-white font-light transition-all focus-visible:bg-black focus-visible:text-white hover:bg-black hover:text-white focus-visible:translate-x-0 hover:translate-x-0 rounded-md bflag outline-none'>
+									{title}
+								</Link>
+							</li>
+						);
+					})}
+				</menu>
+			</div>
 		</li>
 	);
 };
