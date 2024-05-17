202c4907660d39b33e0b6076e981665b2690b073
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2024-05-16 19:51:40 -0700

    Rewrite cf handler so it throws to a global catch

--- src/app/cf/route.ts
+++ src/app/cf/route.ts
@@ -1,43 +1,58 @@
 import { NextRequest } from 'next/server';
 import { htmlStringer, plaintextStringer, validator } from './_lib';
+import { createTransport } from 'nodemailer';
 
 export async function POST(req: NextRequest) {
 	try {
-		const json = await req.json();
-		const parsed = validator(json);
-
-		// Data validation
-		if (parsed === false) {
-			return Response.json({ message: 'Not found' }, { status: 404 });
+		let parsed;
+		try {
+			// Data validation
+			const json = await req.json();
+			parsed = validator(json);
+		} catch {
+			throw {
+				message: 'Something happened with the request, try again',
+				status: 400,
+			};
+		}
+		if (typeof parsed === 'boolean') {
+			throw { message: 'Not found', status: 404 };
 		}
 
 		// ReCAPTCHA validation
-		const { threeToken } = parsed;
-		const ReCAPTCHAResponse = await fetch(
-			`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.PRIVATE_CONTACT_FORM_RECAPTCHA_KEY}&response=${threeToken}`,
-		);
-		const ReCAPTCHAData = await ReCAPTCHAResponse.json();
-		// Only valid error to report to user is if they took too long
-		if (
-			ReCAPTCHAData['error-codes'] !== undefined &&
-			ReCAPTCHAData['error-codes'].includes('timeout-or-duplicate') === true
-		) {
-			return Response.json(
-				{ message: 'ReCAPTCHA response expired. Please submit again.' },
-				{ status: 502 },
+		let ReCAPTCHAData;
+		try {
+			const { threeToken } = parsed;
+			const ReCAPTCHAResponse = await fetch(
+				`https://www.google.com/recaptcha/api/siteverify?secret=${process.env.PRIVATE_CONTACT_FORM_RECAPTCHA_KEY}&response=${threeToken}`,
 			);
+			ReCAPTCHAData = await ReCAPTCHAResponse.json();
+			// Only valid error to report to user is if they took too long
+		} catch {
+			throw {
+				message: 'Something happened with the server; try again',
+				status: 502,
+			};
 		}
+		if (
+			ReCAPTCHAData['error-codes'] &&
+			ReCAPTCHAData['error-codes'].includes('timeout-or-duplicate')
+		)
+			throw {
+				message: 'ReCAPTCHA response expired. Please submit again.',
+				status: 502,
+			};
+
 		// If request fails at network level?
 		// Arbritrary score indicaitng bot?
 		if (ReCAPTCHAData.score < 0.3 || ReCAPTCHAData.success === false) {
-			return Response.json({ message: 'Not found' }, { status: 404 });
+			throw { message: 'Not found', status: 404 };
 		}
 
 		// Emailer
 		const { name, email } = parsed;
-		const mailer = require('nodemailer');
 		// Some gmail docs indicate the correct smtp settings / procedure for handshakes
-		const transport = mailer.createTransport({
+		const transport = createTransport({
 			host: 'smtp.gmail.com',
 			port: 587,
 			secure: false,
@@ -50,26 +65,21 @@ export async function POST(req: NextRequest) {
 		// Unknown if this works
 		transport.verify((error: any, success: any) => {
 			if (error) {
-				return Response.json(
-					{ message: 'Server error. Submit your info again.' },
-					{ status: 502 },
-				);
+				throw { message: 'Server error. Submit your info again.', status: 502 };
 			}
 		});
 		// Send both plaintext and HTML; HTML from stringer function
 		const emailMessage = {
-			from: process.env.PRIVATE_EMAIL_USER,
+			from: process.env.PRIVATE_EMAIL_USER!,
 			to: email,
 			subject: 'Hello from Nicolas Vo!',
 			text: plaintextStringer(name),
 			html: htmlStringer(name),
 		};
-		const sendResult = await transport.sendMail(emailMessage);
-		if (sendResult.err) {
-			return Response.json(
-				{ message: 'Server rror. Submit your info again.' },
-				{ status: 502 },
-			);
+		try {
+			await transport.sendMail(emailMessage);
+		} catch {
+			throw { message: 'Server error. Submit your info again.', status: 502 };
 		}
 		return Response.json(
 			{ message: `Received. An email confirmation should arrive soon.` },
