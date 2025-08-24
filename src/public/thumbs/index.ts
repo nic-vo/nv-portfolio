e9e06b9efc5b7c802a880dc1c78e66d797b749c2
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2025-08-23 22:26:19 -0400

    chore: remove thumbs for trivial projects

--- src/public/thumbs/index.ts
+++ src/public/thumbs/index.ts
@@ -1,17 +1,5 @@
 import BlockBuilders from './BlockBuilders.webp';
-import Calculator from './Calculator.webp';
-import DrumMachine from './DrumMachine.webp';
-import LofiCafe from './LofiCafe.webp';
-import Markdown from './Markdown.webp';
 import MixDelta from './MixDelta.webp';
 import PortfolioSite from './PortfolioSite.webp';
 
-export {
-	BlockBuilders,
-	Calculator,
-	DrumMachine,
-	LofiCafe,
-	Markdown,
-	MixDelta,
-	PortfolioSite,
-};
+export { BlockBuilders, MixDelta, PortfolioSite };
