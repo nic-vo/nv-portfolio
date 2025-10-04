3e7bab42adeaecdfa91ad2d0ea5e8ad5543a6626
Nicolas Vo <98785291+nic-vo@users.noreply.github.com>
2025-10-04 18:01:32 -0400

    feat: add some global components

--- src/components/global.tsx
+++ src/components/global.tsx
@@ -1,5 +1,33 @@
-import { PropsWithChildren } from 'react';
+import { DetailedHTMLProps, HTMLAttributes, PropsWithChildren } from 'react';
+import { twMerge } from 'tailwind-merge';
+
+/**
+ * For instances where I want to reproduce styling
+ * but otherwise keep an element customizable. \
+ * */
+type SCEC<T = HTMLElement> = PropsWithChildren &
+    DetailedHTMLProps<HTMLAttributes<T>, T>;
 
 export const SRText = (props: PropsWithChildren) => {
     return <span className='sr-only'>{props.children}</span>;
 };
+
+export const AppMain = ({ children, className, ...attrs }: SCEC) => (
+    <main
+        className={twMerge('', className)}
+        {...attrs}>
+        {children}
+    </main>
+);
+
+export const HeadingOne = ({
+    children,
+    className,
+    ...attrs
+}: SCEC<HTMLHeadingElement>) => (
+    <h1
+        className={twMerge('', className)}
+        {...attrs}>
+        {children}
+    </h1>
+);
